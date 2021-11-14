2e4de9757ea5846caca5ada88e7efab382408754
Bert Jorissen <info@bertjorissen.be>
2021-11-14 22:24:27 +0100

    Items-Blocks
    -lang-file nl_be
    -classes for Blocks/Items/ItemGroups
    -cleaned up the Main

---
+++ src/main/java/be/uantwerpen/scicraft/block/Blocks.java
@@ -0,0 +1,10 @@
+package be.uantwerpen.scicraft.block;
+
+import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
+import net.minecraft.block.Block;
+import net.minecraft.block.MapColor;
+import net.minecraft.block.Material;
+
+public class Blocks {
+    public static final Block PROTON_BLOCK = new Block(FabricBlockSettings.of(Material.WOOL).mapColor(MapColor.WHITE).strength(2f).nonOpaque().collidable(false));
+}
---
+++ src/main/java/be/uantwerpen/scicraft/item/Items.java
@@ -0,0 +1,11 @@
+package be.uantwerpen.scicraft.item;
+
+import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+import net.minecraft.item.BlockItem;
+import net.minecraft.item.Item;
+import be.uantwerpen.scicraft.block.Blocks;
+
+public class Items {
+    // BlockItems
+    public static final Item PROTON = new BlockItem(Blocks.PROTON_BLOCK, new FabricItemSettings().group(ItemGroup.QUANTUM_FIELDS));
+}
