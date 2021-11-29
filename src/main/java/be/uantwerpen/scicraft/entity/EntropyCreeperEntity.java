1791db79a0aef1c6234ba2214888814c863fbc5a
Joey De Pauw <joeydepauw@gmail.com>
2021-11-29 20:41:01 +0100

    changed redirect to inject, which are safer.

--- src/main/java/be/uantwerpen/scicraft/entity/EntropyCreeperEntity.java
+++ src/main/java/be/uantwerpen/scicraft/entity/EntropyCreeperEntity.java
@@ -1,5 +1,6 @@
 package be.uantwerpen.scicraft.entity;
 
+import be.uantwerpen.scicraft.Scicraft;
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.mob.CreeperEntity;
 import net.minecraft.world.World;
@@ -11,15 +12,19 @@ public class EntropyCreeperEntity extends CreeperEntity {
     }
 
     /**
-     * New On Explode method
+     * Hook into explode method
      * Thanks Mixin!
-     * <p>
-     * Make sure to "kill" the entity
-     * TODO custom explosion
      */
-    public void onExplode() {
-        this.dead = true;
+    public boolean preExplode() {
         // BOOM !!
+        this.dead = true;
+        this.discard();
+        Scicraft.LOGGER.info("Entropy creeper exploded");
+
+        // TODO: Custom exposion
+
+        return false;   // make sure the original 'explode' function doesn't run.
     }
+
 
 }
--- src/main/java/be/uantwerpen/scicraft/mixins/CreeperEntityMixin.java
+++ src/main/java/be/uantwerpen/scicraft/mixins/CreeperEntityMixin.java
@@ -1,42 +1,20 @@
 package be.uantwerpen.scicraft.mixins;
 
-import be.uantwerpen.scicraft.entity.EntropyCreeperEntity;
-import net.minecraft.entity.AreaEffectCloudEntity;
-import net.minecraft.entity.EntityType;
-import net.minecraft.entity.effect.StatusEffectInstance;
 import net.minecraft.entity.mob.CreeperEntity;
-import net.minecraft.entity.mob.HostileEntity;
-import net.minecraft.world.GameRules;
-import net.minecraft.world.World;
-import net.minecraft.world.explosion.Explosion;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.gen.Accessor;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Redirect;
-
-import java.util.Collection;
-import java.util.Iterator;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 /**
  * CreeperEntityMixin change Base minecraft code for own use
  *
- * @author pixar02
- * <p>
+ * @author pixar02, JoeyDP
  * Define what class you want to Mixin (Mix in too)
  */
 @Mixin(CreeperEntity.class)
-public abstract class CreeperEntityMixin extends HostileEntity {
-
-    /**
-     * Hostile entity constructor
-     * Just super call, allows for access to public/protected fields/methods
-     *
-     * @param entityType : EntityType that is of HostileEntity
-     * @param world : world location of entity
-     */
-    protected CreeperEntityMixin(EntityType<? extends HostileEntity> entityType, World world) {
-        super(entityType, world);
-    }
+public abstract class CreeperEntityMixin {
 
     /**
      * Get the Explosion Radius from Creeper Entity
@@ -48,60 +26,24 @@ public abstract class CreeperEntityMixin extends HostileEntity {
     public abstract int getExplosionRadius();
 
     /**
-     * RedirectExplode
+     * Method that is called before the creeper type mob explodes.
+     * Can return true to continue with original explosion code or false to skip it.
      *
-     * Redirect the explosion call from CreeperEntity in tick() to onExplode(...)
-     * Allows a change in the code
-     *
-     * @param creeperEntity : creeper entity that is about to explode
+     * @return Whether to run original explode code.
      */
-    @Redirect(method = "tick",
-            at = @At(value = "INVOKE", target = "Lnet/minecraft/entity/mob/CreeperEntity;explode()V"))
-    public void redirectExplode(CreeperEntity creeperEntity) {
-        this.onExplode(creeperEntity);
+    public boolean preExplode() {
+        return true;
     }
 
     /**
-     * onExplode is called when a creeper is going to explode (Mixin redirect)
-     *
-     * @param entity : CreeperEntity or any derived class of it
+     * Inject a call to onExplode at the beginning of the explode function.
+     * This determines whether the body is executed.
      */
-    public void onExplode(CreeperEntity entity) {
-        if (!entity.world.isClient) {
-            if (entity instanceof EntropyCreeperEntity entropyCreeper) {
-                entropyCreeper.onExplode();
-            } else {
-                Explosion.DestructionType destructionType = this.world.getGameRules().getBoolean(GameRules.DO_MOB_GRIEFING) ? Explosion.DestructionType.DESTROY : Explosion.DestructionType.NONE;
-                float f = entity.shouldRenderOverlay() ? 2.0F : 1.0F;
-                this.dead = true;
-                this.world.createExplosion(entity, this.getX(), this.getY(), this.getZ(), (float) getExplosionRadius() * f, destructionType);
-            }
-            this.discard();
-            this.spawnEffectsCloud();
+    @Inject(at = @At("HEAD"), method = "explode", cancellable = true)
+    public void injectOnExplode(CallbackInfo info) {
+        if (!preExplode()) {
+            info.cancel();
         }
     }
 
-    /**
-     * SpawnEffectsCloud from CreeperEntity
-     * Private in default class ('overriding like this')
-     */
-    public void spawnEffectsCloud() {
-        Collection<StatusEffectInstance> collection = this.getStatusEffects();
-        if (!collection.isEmpty()) {
-            AreaEffectCloudEntity areaEffectCloudEntity = new AreaEffectCloudEntity(this.world, this.getX(), this.getY(), this.getZ());
-            areaEffectCloudEntity.setRadius(2.5F);
-            areaEffectCloudEntity.setRadiusOnUse(-0.5F);
-            areaEffectCloudEntity.setWaitTime(10);
-            areaEffectCloudEntity.setDuration(areaEffectCloudEntity.getDuration() / 2);
-            areaEffectCloudEntity.setRadiusGrowth(-areaEffectCloudEntity.getRadius() / (float) areaEffectCloudEntity.getDuration());
-            Iterator var3 = collection.iterator();
-
-            while (var3.hasNext()) {
-                StatusEffectInstance statusEffectInstance = (StatusEffectInstance) var3.next();
-                areaEffectCloudEntity.addEffect(new StatusEffectInstance(statusEffectInstance));
-            }
-
-            this.world.spawnEntity(areaEffectCloudEntity);
-        }
-    }
 }
