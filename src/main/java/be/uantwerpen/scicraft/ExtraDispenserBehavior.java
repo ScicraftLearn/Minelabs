6711b1fd773a82d337d0112b655b6002b56cd3c1
Thomas Van Daele <thomas.vandaele@student.uantwerpen.be>
2021-12-01 13:59:28 +0100

    Dispenser can shoot electrons

---
+++ src/main/java/be/uantwerpen/scicraft/ExtraDispenserBehavior.java
@@ -0,0 +1,35 @@
+package be.uantwerpen.scicraft;
+
+import be.uantwerpen.scicraft.entity.ElectronEntity;
+import be.uantwerpen.scicraft.item.Items;
+import net.minecraft.block.DispenserBlock;
+import net.minecraft.block.dispenser.ProjectileDispenserBehavior;
+import net.minecraft.entity.projectile.ProjectileEntity;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.Util;
+import net.minecraft.util.math.Position;
+import net.minecraft.world.World;
+
+public class ExtraDispenserBehavior {
+    /**
+     * Main class method
+     * Registers all dispenser behaviors
+     */
+    public static void registerBehaviors() {
+        /**
+         * Register dispenser behavior for shooting out electrons
+         * Implements abstract class ProjectileDispenserBehavior to create the correct entity
+         *
+         * The entity is shot up slight as defined in dispenseSilently in ProjectileDispenserBehavior:
+         * direction.getOffsetY() + 0.1F
+         */
+        DispenserBlock.registerBehavior(Items.ELECTRON_ITEM, new ProjectileDispenserBehavior() {
+            @Override
+            protected ProjectileEntity createProjectile(World world, Position position, ItemStack stack) {
+                return Util.make(new ElectronEntity(world, position.getX(), position.getY(), position.getZ()), (electronEntity) -> {
+                    electronEntity.setItem(stack);
+                });
+            }
+        });
+    }
+}
