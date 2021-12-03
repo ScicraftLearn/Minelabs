86a56d82d99f417e09d62bab5df698b6c7f2b39b
Thomas Van Daele <thomas.vandaele@student.uantwerpen.be>
2021-12-03 12:50:04 +0100

    update mixin to inject

--- src/main/java/be/uantwerpen/scicraft/mixins/ThrownEntityMixin.java
+++ src/main/java/be/uantwerpen/scicraft/mixins/ThrownEntityMixin.java
@@ -1,11 +1,19 @@
 package be.uantwerpen.scicraft.mixins;
 
+import be.uantwerpen.scicraft.Scicraft;
 import be.uantwerpen.scicraft.entity.ElectronEntity;
+import net.minecraft.entity.EntityType;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.projectile.ProjectileEntity;
 import net.minecraft.entity.projectile.thrown.ThrownEntity;
+import net.minecraft.util.hit.HitResult;
 import net.minecraft.util.math.Vec3d;
+import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Redirect;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
 
 /**
  * Mixin that hooks into the tick() method of ThrownEntity
@@ -16,11 +24,18 @@ import org.spongepowered.asm.mixin.injection.Redirect;
  * Not executing setVelocity makes the ThrownEntity not slowdown over time
  */
 @Mixin(ThrownEntity.class)
-public class ThrownEntityMixin {
-    @Redirect(method = "tick()V", at = @At(value = "INVOKE", target = "Lnet/minecraft/entity/projectile/thrown/ThrownEntity;setVelocity(Lnet/minecraft/util/math/Vec3d;)V"))
-    private void injected(ThrownEntity entity, Vec3d velocity) {
-        if (!(entity instanceof ElectronEntity)) {
-            entity.setVelocity(velocity);
+public abstract class ThrownEntityMixin extends ProjectileEntity{
+    protected ThrownEntityMixin(EntityType<? extends ProjectileEntity> entityType, World world) {
+        super(entityType, world);
+    }
+
+    @Inject(method = "tick()V", at = @At(value = "INVOKE", target = "Lnet/minecraft/entity/projectile/thrown/ThrownEntity;setVelocity(Lnet/minecraft/util/math/Vec3d;)V"), locals = LocalCapture.CAPTURE_FAILHARD, cancellable = true)
+    private void injected(CallbackInfo ci, HitResult hitResult, boolean bl, Vec3d blockPos, double blockState, double d, double e, float g) {
+        ProjectileEntity self = this;
+        if (self instanceof ElectronEntity) {
+            Scicraft.LOGGER.info("Ding dong IDE you are wrong");
+            this.setPosition(blockState, d, e);
+            ci.cancel();
         }
     }
 }
