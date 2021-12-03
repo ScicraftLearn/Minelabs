cdf8e841624c55cb1f0fa874953e14d75c99852e
Thomas Van Daele <thomas.vandaele@student.uantwerpen.be>
2021-12-03 13:09:45 +0100

    update documentation of mixin + add some testing

--- src/main/java/be/uantwerpen/scicraft/mixins/ThrownEntityMixin.java
+++ src/main/java/be/uantwerpen/scicraft/mixins/ThrownEntityMixin.java
@@ -3,7 +3,6 @@ package be.uantwerpen.scicraft.mixins;
 import be.uantwerpen.scicraft.Scicraft;
 import be.uantwerpen.scicraft.entity.ElectronEntity;
 import net.minecraft.entity.EntityType;
-import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.projectile.ProjectileEntity;
 import net.minecraft.entity.projectile.thrown.ThrownEntity;
 import net.minecraft.util.hit.HitResult;
@@ -15,25 +14,30 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
 
-/**
- * Mixin that hooks into the tick() method of ThrownEntity
- * This method redirects the following function call: this.setVelocity(blockPos.multiply((double)g));
- * An additional check is added if the entity(this) is of type ElectronEntity
- * Based on this check the setVelocity method is executed or not
- *
- * Not executing setVelocity makes the ThrownEntity not slowdown over time
- */
 @Mixin(ThrownEntity.class)
 public abstract class ThrownEntityMixin extends ProjectileEntity{
     protected ThrownEntityMixin(EntityType<? extends ProjectileEntity> entityType, World world) {
         super(entityType, world);
     }
 
+    /**
+     * Mixin that hooks into the tick() method of ThrownEntity
+     * This method injects before the following function call: this.setVelocity(blockPos.multiply((double)g));
+     * First a check is done if the entity is of type ElectronEntity
+     * If this is the case we update the position without changing the velocity after that we cancel the original method.
+     * Else just continue running the original method.
+     */
     @Inject(method = "tick()V", at = @At(value = "INVOKE", target = "Lnet/minecraft/entity/projectile/thrown/ThrownEntity;setVelocity(Lnet/minecraft/util/math/Vec3d;)V"), locals = LocalCapture.CAPTURE_FAILHARD, cancellable = true)
     private void injected(CallbackInfo ci, HitResult hitResult, boolean bl, Vec3d blockPos, double blockState, double d, double e, float g) {
         ProjectileEntity self = this;
+        // IDE might say the following expression is always false.
+        // This is not the case, I tested it ingame.
         if (self instanceof ElectronEntity) {
-            Scicraft.LOGGER.info("Ding dong IDE you are wrong");
+            // I used command: /tp @p 0 100 0 0 0
+            // This teleports player to block x=0 y=100 z=0 and sets the looking direction of the player
+            // Then don't move and use an electron. It is easy to verify that the electron flies straight and constant speed.
+            Scicraft.LOGGER.info(this.getVelocity());
+            Scicraft.LOGGER.info(this.getPos());
             this.setPosition(blockState, d, e);
             ci.cancel();
         }
