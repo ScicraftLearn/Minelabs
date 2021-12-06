b01795bd726592ed5afd8e165a521e8cef9b5a93
pixar02 <pixar.pf@gmail.com>
2021-12-06 23:43:24 +0100

    Unnecessary imports

--- src/main/java/be/uantwerpen/scicraft/mixins/CreeperEntityMixin.java
+++ src/main/java/be/uantwerpen/scicraft/mixins/CreeperEntityMixin.java
@@ -2,8 +2,6 @@ package be.uantwerpen.scicraft.mixins;
 
 import net.minecraft.entity.mob.CreeperEntity;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
-import org.spongepowered.asm.mixin.gen.Accessor;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
