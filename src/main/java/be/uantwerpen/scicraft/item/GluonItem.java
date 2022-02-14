531f968cd0ecc04326ef171414c3c36ee436d714
zlatko <Zlatko.Ristic@student.uantwerpen.be>
2022-02-14 11:55:03 +0100

    Added gluon as item and it workd

---
+++ src/main/java/be/uantwerpen/scicraft/item/GluonItem.java
@@ -0,0 +1,9 @@
+package be.uantwerpen.scicraft.item;
+
+import net.minecraft.item.Item;
+
+public class GluonItem extends Item {
+    public GluonItem(Settings settings) {
+        super(settings);
+    }
+}
