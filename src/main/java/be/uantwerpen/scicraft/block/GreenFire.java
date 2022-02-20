f15c4134730861e33932001bc790223700d07448
pixar02 <pixar.pf@gmail.com>
2022-02-20 15:52:59 +0100

    cleanup + waiting for ART

--- src/main/java/be/uantwerpen/scicraft/block/GreenFire.java
+++ src/main/java/be/uantwerpen/scicraft/block/GreenFire.java
@@ -9,21 +9,53 @@ import net.minecraft.util.math.Direction;
 import net.minecraft.world.WorldAccess;
 import net.minecraft.world.WorldView;
 
+/**
+ * TODO ART + Animation
+ * @author pixar02
+ */
 public class GreenFire extends AbstractFireBlock {
 
     public GreenFire(Settings settings, float damage) {
         super(settings, damage);
     }
 
+    /**
+     * Check if the lit block is of copper
+     *
+     * @param state : stat of the base
+     *
+     * @return boolean
+     */
     public static boolean isCopperBase(BlockState state){
         return state.isIn(Tags.Blocks.COPPER_BLOCKS);
     }
 
+    /**
+     *
+     * @param state : current state of block
+     * @param direction: Direction of the block
+     * @param neighborState : BlockState of neighbor
+     * @param world : world of the block
+     * @param pos : pos of block
+     * @param neighborPos : position of neighbor
+     *
+     * @deprecated No way around it....
+     *
+     * @return {@link BlockState} what state to place
+     */
     @Override
     public BlockState getStateForNeighborUpdate(BlockState state, Direction direction, BlockState neighborState, WorldAccess world, BlockPos pos, BlockPos neighborPos) {
         return this.canPlaceAt(state, world, pos) ? this.getDefaultState() : Blocks.AIR.getDefaultState();
     }
 
+    /**
+     * Can we place this block?
+     *
+     * @param state : current BlockState
+     * @param world : world we are placing in
+     * @param pos : position we are placing at
+     * @return boolean
+     */
     @Override
     public boolean canPlaceAt(BlockState state, WorldView world, BlockPos pos) {
         return isCopperBase(world.getBlockState(pos.down()));
