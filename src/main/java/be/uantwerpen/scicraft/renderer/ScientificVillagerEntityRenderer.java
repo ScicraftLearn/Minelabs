816f4b3670d75f029f8a49b5807812c4f71a8ca7
zlatko <Zlatko.Ristic@student.uantwerpen.be>
2022-02-22 14:44:45 +0100

    They don't render good

--- src/main/java/be/uantwerpen/scicraft/renderer/ScientificVillagerEntityRenderer.java
+++ src/main/java/be/uantwerpen/scicraft/renderer/ScientificVillagerEntityRenderer.java
@@ -1,16 +1,7 @@
 package be.uantwerpen.scicraft.renderer;
 
-import be.uantwerpen.scicraft.ScicraftClient;
-import be.uantwerpen.scicraft.entity.Entities;
-import be.uantwerpen.scicraft.entity.ScientificVillagerEntity;
 import net.minecraft.client.render.entity.EntityRendererFactory;
-import net.minecraft.client.render.entity.MobEntityRenderer;
 import net.minecraft.client.render.entity.VillagerEntityRenderer;
-import net.minecraft.client.render.entity.feature.HeadFeatureRenderer;
-import net.minecraft.client.render.entity.feature.VillagerClothingFeatureRenderer;
-import net.minecraft.client.render.entity.feature.VillagerHeldItemFeatureRenderer;
-import net.minecraft.client.render.entity.model.EntityModelLayers;
-import net.minecraft.client.render.entity.model.VillagerResemblingModel;
 import net.minecraft.entity.passive.VillagerEntity;
 import net.minecraft.util.Identifier;
 
