6a41004f2e21c50e43198b877e0c6f24659b817d
zlatko <Zlatko.Ristic@student.uantwerpen.be>
2022-03-05 16:10:06 +0100

    init commit

--- src/main/java/be/uantwerpen/scicraft/item/Items.java
+++ src/main/java/be/uantwerpen/scicraft/item/Items.java
@@ -23,6 +23,7 @@ public class Items {
     public static final Item ENTROPY_CREEPER_SPAWN_EGG = register(new SpawnEggItem(Entities.ENTROPY_CREEPER,
             0xbb64e1, 0x5d0486, new FabricItemSettings().group(ItemGroup.MISC)), "entropy_creeper_spawn_egg");
 
+    public static final Item SPYGLASS = register(new Item(new Item.Settings().group(ItemGroup.TOOLS).maxCount(1)), "spyglass");
     // Items > Atoms
     public static final Item HYDROGEN_ATOM = register(new AtomItem(new Item.Settings().group(ItemGroups.SCICRAFT), 1, "H"), "hydrogen_atom");
     public static final Item HELIUM_ATOM = register(new AtomItem(new Item.Settings().group(ItemGroups.SCICRAFT), 2, "He"), "helium_atom");
---
+++ src/main/java/be/uantwerpen/scicraft/item/SpyGlass.java
@@ -0,0 +1,14 @@
+package be.uantwerpen.scicraft.item;
+
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.inventory.StackReference;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.SpyglassItem;
+import net.minecraft.screen.slot.Slot;
+import net.minecraft.util.ClickType;
+
+public class SpyGlass extends SpyglassItem {
+    public SpyGlass(Settings settings){
+        super(settings);
+    }
+}
