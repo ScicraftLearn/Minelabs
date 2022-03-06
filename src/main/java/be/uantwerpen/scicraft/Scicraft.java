08cf539cb04d1899a6272d35b16de6a44cc82541
JoshuaClaes <joshua.claes38@gmail.com>
2022-03-06 12:06:59 +0100

    Removed unnecessary lines

--- src/main/java/be/uantwerpen/scicraft/Scicraft.java
+++ src/main/java/be/uantwerpen/scicraft/Scicraft.java
@@ -6,20 +6,9 @@ import be.uantwerpen.scicraft.item.Items;
 import be.uantwerpen.scicraft.sound.SoundEvents;
 import be.uantwerpen.scicraft.world.gen.WorldGen;
 import net.fabricmc.api.ModInitializer;
-import net.fabricmc.fabric.api.biome.v1.BiomeModifications;
-import net.fabricmc.fabric.api.biome.v1.BiomeSelectors;
-import net.minecraft.util.Identifier;
-import net.minecraft.util.registry.BuiltinRegistries;
-import net.minecraft.util.registry.Registry;
-import net.minecraft.util.registry.RegistryKey;
-import net.minecraft.world.gen.GenerationStep;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import net.minecraft.world.gen.YOffset;
-import net.minecraft.world.gen.decorator.CountPlacementModifier;
-import net.minecraft.world.gen.decorator.HeightRangePlacementModifier;
-import net.minecraft.world.gen.decorator.SquarePlacementModifier;
-import net.minecraft.world.gen.feature.*;
+
 
 
 public class Scicraft implements ModInitializer {
