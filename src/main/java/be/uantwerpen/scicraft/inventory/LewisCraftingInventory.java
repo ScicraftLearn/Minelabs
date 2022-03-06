76582f231eebcc2583cf80bed9cc33f43daa780b
CedricLeclercq <cedric.leclercq@icloud.com>
2022-03-06 10:50:19 +0100

    Inventory changes

--- src/main/java/be/uantwerpen/scicraft/inventory/LewisCraftingInventory.java
+++ src/main/java/be/uantwerpen/scicraft/inventory/LewisCraftingInventory.java
@@ -4,123 +4,93 @@ import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.inventory.Inventories;
 import net.minecraft.inventory.Inventory;
 import net.minecraft.item.ItemStack;
+import net.minecraft.recipe.RecipeInputProvider;
+import net.minecraft.recipe.RecipeMatcher;
+import net.minecraft.screen.ScreenHandler;
 import net.minecraft.util.collection.DefaultedList;
 
-public interface LewisCraftingInventory extends Inventory {
-    /**
-     * Retrieves the item list of this inventory.
-     * Must return the same instance every time it's called.
-     */
-    DefaultedList<ItemStack> getItems();
+public class LewisCraftingInventory implements Inventory, RecipeInputProvider {
 
-    /**
-     * Creates an inventory from the item list.
-     */
-    static LewisCraftingInventory of(DefaultedList<ItemStack> items) {
-        return () -> items;
+    private final DefaultedList<ItemStack> stacks;
+    private final int width;
+    private final int height;
+    private final ScreenHandler handler;
+
+    public LewisCraftingInventory(DefaultedList<ItemStack> stacks, int width, int height, ScreenHandler handler) {
+        this.stacks = stacks;
+        this.width = width;
+        this.height = height;
+        this.handler = handler;
     }
 
-    /**
-     * Creates a new inventory with the specified size.
-     */
-    static LewisCraftingInventory ofSize(int size) {
-        return of(DefaultedList.ofSize(size, ItemStack.EMPTY));
-    }
-
-    /**
-     * Returns the inventory size.
-     */
     @Override
-    default int size() {
-        return getItems().size();
+    public int size() {
+        return this.stacks.size();
     }
 
-    /**
-     * Checks if the inventory is empty.
-     * @return true if this inventory has only empty stacks, false otherwise.
-     */
     @Override
-    default boolean isEmpty() {
-        for (int i = 0; i < size(); i++) {
-            ItemStack stack = getStack(i);
-            if (!stack.isEmpty()) {
-                return false;
-            }
+    public boolean isEmpty() {
+        for (ItemStack itemStack : this.stacks) {
+            if (itemStack.isEmpty()) continue;
+            return false;
         }
         return true;
     }
 
-    /**
-     * Retrieves the item in the slot.
-     */
     @Override
-    default ItemStack getStack(int slot) {
-        return getItems().get(slot);
-    }
-
-    /**
-     * Removes items from an inventory slot.
-     * @param slot  The slot to remove from.
-     * @param count How many items to remove. If there are less items in the slot than what are requested,
-     *              takes all items in that slot.
-     */
-    @Override
-    default ItemStack removeStack(int slot, int count) {
-        ItemStack result = Inventories.splitStack(getItems(), slot, count);
-        if (!result.isEmpty()) {
-            markDirty();
+    public ItemStack getStack(int slot) {
+        if (slot >= this.size()) {
+            return ItemStack.EMPTY;
         }
-        return result;
+        return this.stacks.get(slot);
     }
 
-    /**
-     * Removes all items from an inventory slot.
-     * @param slot The slot to remove from.
-     */
     @Override
-    default ItemStack removeStack(int slot) {
-        return Inventories.removeStack(getItems(), slot);
+    public ItemStack removeStack(int slot) {
+        return Inventories.removeStack(this.stacks, slot);
     }
 
-    /**
-     * Replaces the current stack in an inventory slot with the provided stack.
-     * @param slot  The inventory slot of which to replace the itemstack.
-     * @param stack The replacing itemstack. If the stack is too big for
-     *              this inventory ({@link Inventory#getMaxCountPerStack()}),
-     *              it gets resized to this inventory's maximum amount.
-     */
     @Override
-    default void setStack(int slot, ItemStack stack) {
-        getItems().set(slot, stack);
-        if (stack.getCount() > getMaxCountPerStack()) {
-            stack.setCount(getMaxCountPerStack());
+    public ItemStack removeStack(int slot, int amount) {
+        ItemStack itemStack = Inventories.splitStack(this.stacks, slot, amount);
+        if (!itemStack.isEmpty()) {
+            this.handler.onContentChanged(this);
         }
+        return itemStack;
     }
 
-    /**
-     * Clears the inventory.
-     */
     @Override
-    default void clear() {
-        getItems().clear();
+    public void setStack(int slot, ItemStack stack) {
+        this.stacks.set(slot, stack);
+        this.handler.onContentChanged(this);
     }
 
-    /**
-     * Marks the state as dirty.
-     * Must be called after changes in the inventory, so that the game can properly save
-     * the inventory contents and notify neighboring blocks of inventory changes.
-     */
     @Override
-    default void markDirty() {
-        // Override if you want behavior.
+    public void markDirty() {
     }
 
-    /**
-     * @return true if the player can use the inventory, false otherwise.
-     */
     @Override
-    default boolean canPlayerUse(PlayerEntity player) {
+    public boolean canPlayerUse(PlayerEntity player) {
         return true;
     }
 
+    @Override
+    public void clear() {
+        this.stacks.clear();
+    }
+
+    public int getHeight() {
+        return this.height;
+    }
+
+    public int getWidth() {
+        return this.width;
+    }
+
+    @Override
+    public void provideRecipeInputs(RecipeMatcher finder) {
+        for (ItemStack itemStack : this.stacks) {
+            finder.addUnenchantedInput(itemStack);
+        }
+    }
 }
