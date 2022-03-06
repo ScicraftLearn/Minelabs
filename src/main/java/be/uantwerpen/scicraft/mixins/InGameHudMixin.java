b10af88c8fa7afb4a96949b69b83b04a32450f15
zlatko <Zlatko.Ristic@student.uantwerpen.be>
2022-03-06 12:14:59 +0100

    Loads a dummy texture in when looking at a block within distance

--- src/main/java/be/uantwerpen/scicraft/mixins/InGameHudMixin.java
+++ src/main/java/be/uantwerpen/scicraft/mixins/InGameHudMixin.java
@@ -1,15 +1,13 @@
 package be.uantwerpen.scicraft.mixins;
 
+import com.mojang.blaze3d.systems.RenderSystem;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
 import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.gui.hud.InGameHud;
 import net.minecraft.entity.player.PlayerEntity;
-import net.minecraft.item.ItemStack;
-import net.minecraft.item.SpyglassItem;
 import net.minecraft.text.LiteralText;
-import net.minecraft.text.Text;
-import net.minecraft.util.Hand;
-import net.minecraft.util.TypedActionResult;
+import net.minecraft.util.Identifier;
 import net.minecraft.util.hit.BlockHitResult;
 import net.minecraft.util.hit.HitResult;
 import net.minecraft.util.math.BlockPos;
@@ -17,14 +15,15 @@ import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-import java.util.Objects;
-
-@Mixin(SpyglassItem.class)
-public class SpyGlassItemMixin {
-    @Inject(method="use", at= @At(value = "RETURN"))
-    private void use(World world, PlayerEntity user, Hand hand, CallbackInfoReturnable<TypedActionResult<ItemStack>> cir) {
+@Mixin(InGameHud.class)
+public class InGameHudMixin {
+    @Inject(method = "renderSpyglassOverlay", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/render/Tessellator;getInstance()Lnet/minecraft/client/render/Tessellator;"))
+    private void renderSpyglassOverlay(float scale, CallbackInfo ci) {
+        final Identifier TESTING = new Identifier("scicraft:textures/block/electron.png");
+        PlayerEntity user = MinecraftClient.getInstance().player;
+        World world = MinecraftClient.getInstance().world;
         HitResult lookingAt = MinecraftClient.getInstance().crosshairTarget;
         BlockPos blockPos = null;
         //This looks if what you are pointing at is a block.
@@ -33,13 +32,12 @@ public class SpyGlassItemMixin {
             blockPos = blockHit.getBlockPos();
         }
         //If lookingAt is not a block within range, do normal SpyGlass.
-        if(blockPos != null && blockPos.isWithinDistance(user.getPos(), 1.0)){
+        if (blockPos != null && blockPos.isWithinDistance(user.getPos(), 2.4)) {
             //Show picture of molecule model.
             BlockState blockState = world.getBlockState(blockPos);
             Block block = blockState.getBlock();
-            if (!world.isClient) {
-                user.sendMessage(new LiteralText("Hello, atoms"), false);
-            }
+            RenderSystem.setShaderTexture(0, TESTING);
+
         }
     }
 }
