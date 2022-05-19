15df46a8510371ed8ed1101d6aa6e10b820150ab
iarts <ine.arts@uantwerpen.be>
2022-05-19 21:06:34 +0200

    balloon: flying if selected or slot=0

--- src/main/java/be/uantwerpen/scicraft/item/BalloonItem.java
+++ src/main/java/be/uantwerpen/scicraft/item/BalloonItem.java
@@ -1,13 +1,10 @@
 package be.uantwerpen.scicraft.item;
 
-import net.minecraft.client.network.ClientPlayNetworkHandler;
 import net.minecraft.client.network.ClientPlayerEntity;
 import net.minecraft.entity.Entity;
-import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
-import net.minecraft.network.packet.c2s.play.ClientCommandC2SPacket;
 import net.minecraft.world.World;
 
 public class BalloonItem extends Item {
@@ -18,11 +15,19 @@ public class BalloonItem extends Item {
     @Override
     public void inventoryTick(ItemStack itemStack, World world, Entity entity, int slot, boolean selected) {
         super.inventoryTick(itemStack, world, entity, slot, selected);
-        if (selected && slot == 1) {
-            if (world.isClient()) {
-                if (entity instanceof  PlayerEntity) {
-                    ((PlayerEntity) entity).getAbilities().flying = true;
-                    ((ClientPlayerEntity) entity).sendAbilitiesUpdate();
+        if (world.isClient()) {
+            if (entity instanceof  PlayerEntity) {
+                if (!((PlayerEntity) entity).getAbilities().creativeMode) {
+                    //System.out.println(Integer.toString(slot));
+                    if ((selected || slot==0) && !((PlayerEntity) entity).getAbilities().allowFlying) {
+                        ((PlayerEntity) entity).getAbilities().allowFlying = true;
+                        ((ClientPlayerEntity) entity).sendAbilitiesUpdate();
+                        // TODO item takes damage
+                    } else if (!selected && slot!=0 && ((PlayerEntity) entity).getAbilities().allowFlying) {
+                        ((PlayerEntity) entity).getAbilities().allowFlying = false;
+                        ((PlayerEntity) entity).getAbilities().flying = false;
+                        ((ClientPlayerEntity) entity).sendAbilitiesUpdate();
+                    }
                 }
             }
         }
