4464c03b3524daeea41e211aa45b93e8f06733d7
Joey De Pauw <joeydepauw@gmail.com>
2022-06-02 10:28:19 +0200

    disable throwing of electron, neutron and proton items

--- src/main/java/be/uantwerpen/scicraft/item/ElectronItem.java
+++ src/main/java/be/uantwerpen/scicraft/item/ElectronItem.java
@@ -21,39 +21,39 @@ public class ElectronItem extends BlockItem {
     //    return ActionResult.SUCCESS;
     //}
 
-    /**
-     * When ElectronItem is right-clicked, use up the item if necessary and spawn the entity
-     * @param world minecraft world
-     * @param user player invoking the right click action
-     * @param hand the hand of the user
-     * @return TypedActionResult, indicates if the use of the item succeeded or not
-     */
-    @Override
-    public TypedActionResult<ItemStack> use(World world, PlayerEntity user, Hand hand) {
-        ItemStack itemStack = user.getStackInHand(hand); // creates a new ItemStack instance of the user's itemStack in-hand
-
-        /* TODO sound effect of Electron throw
-         * Example with snowball sound
-         * world.playSound(null, user.getX(), user.getY(), user.getZ(), SoundEvents.ENTITY_SNOWBALL_THROW, SoundCategory.NEUTRAL, 0.5F, 1F);
-         */
-
-        /* TODO cooldown on the throw of an electron (like the cooldown on Ender Pearls)
-         * Example cooldown of 5 ticks
-         * user.getItemCooldownManager().set(this, 5);
-         */
-        if (!world.isClient) {
-            // Spawns the electron entity with correct initial velocity (velocity has the same direction as the players looking direction)
-            ElectronEntity electronEntity = new ElectronEntity(world, user);
-            electronEntity.setItem(itemStack);
-            electronEntity.setVelocity(user, user.getPitch(), user.getYaw(), user.getRoll(), 1.5F, 0F);
-            world.spawnEntity(electronEntity);
-        }
-
-        user.incrementStat(Stats.USED.getOrCreateStat(this));
-        if (!user.getAbilities().creativeMode) {
-            itemStack.decrement(1); // decrements itemStack if user is not in creative mode
-        }
-
-        return TypedActionResult.success(itemStack, world.isClient());
-    }
+//    /**
+//     * When ElectronItem is right-clicked, use up the item if necessary and spawn the entity
+//     * @param world minecraft world
+//     * @param user player invoking the right click action
+//     * @param hand the hand of the user
+//     * @return TypedActionResult, indicates if the use of the item succeeded or not
+//     */
+//    @Override
+//    public TypedActionResult<ItemStack> use(World world, PlayerEntity user, Hand hand) {
+//        ItemStack itemStack = user.getStackInHand(hand); // creates a new ItemStack instance of the user's itemStack in-hand
+//
+//        /* TODO sound effect of Electron throw
+//         * Example with snowball sound
+//         * world.playSound(null, user.getX(), user.getY(), user.getZ(), SoundEvents.ENTITY_SNOWBALL_THROW, SoundCategory.NEUTRAL, 0.5F, 1F);
+//         */
+//
+//        /* TODO cooldown on the throw of an electron (like the cooldown on Ender Pearls)
+//         * Example cooldown of 5 ticks
+//         * user.getItemCooldownManager().set(this, 5);
+//         */
+//        if (!world.isClient) {
+//            // Spawns the electron entity with correct initial velocity (velocity has the same direction as the players looking direction)
+//            ElectronEntity electronEntity = new ElectronEntity(world, user);
+//            electronEntity.setItem(itemStack);
+//            electronEntity.setVelocity(user, user.getPitch(), user.getYaw(), user.getRoll(), 1.5F, 0F);
+//            world.spawnEntity(electronEntity);
+//        }
+//
+//        user.incrementStat(Stats.USED.getOrCreateStat(this));
+//        if (!user.getAbilities().creativeMode) {
+//            itemStack.decrement(1); // decrements itemStack if user is not in creative mode
+//        }
+//
+//        return TypedActionResult.success(itemStack, world.isClient());
+//    }
 }
--- src/main/java/be/uantwerpen/scicraft/item/NeutronItem.java
+++ src/main/java/be/uantwerpen/scicraft/item/NeutronItem.java
@@ -16,39 +16,39 @@ public class NeutronItem extends BlockItem {
         super(Blocks.NEUTRON, settings);
     }
 
-    /**
-     * When NeutronItem is right-clicked, use up the item if necessary and spawn the entity
-     * @param world minecraft world
-     * @param user player invoking the right click action
-     * @param hand the hand of the user
-     * @return TypedActionResult, indicates if the use of the item succeeded or not
-     */
-    @Override
-    public TypedActionResult<ItemStack> use(World world, PlayerEntity user, Hand hand) {
-        ItemStack itemStack = user.getStackInHand(hand); // creates a new ItemStack instance of the user's itemStack in-hand
-
-        /* TODO sound effect of Neutron throw
-         * Example with snowball sound
-         * world.playSound(null, user.getX(), user.getY(), user.getZ(), SoundEvents.ENTITY_SNOWBALL_THROW, SoundCategory.NEUTRAL, 0.5F, 1F);
-         */
-
-        /* TODO cooldown on the throw of an neutron (like the cooldown on Ender Pearls)
-         * Example cooldown of 5 ticks
-         * user.getItemCooldownManager().set(this, 5);
-         */
-        if (!world.isClient) {
-            // Spawns the neutron entity with correct initial velocity (velocity has the same direction as the players looking direction)
-            NeutronEntity neutronEntity = new NeutronEntity(world, user);
-            neutronEntity.setItem(itemStack);
-            neutronEntity.setVelocity(user, user.getPitch(), user.getYaw(), user.getRoll(), 1.5F, 0F);
-            world.spawnEntity(neutronEntity);
-        }
-
-        user.incrementStat(Stats.USED.getOrCreateStat(this));
-        if (!user.getAbilities().creativeMode) {
-            itemStack.decrement(1); // decrements itemStack if user is not in creative mode
-        }
-
-        return TypedActionResult.success(itemStack, world.isClient());
-    }
+//    /**
+//     * When NeutronItem is right-clicked, use up the item if necessary and spawn the entity
+//     * @param world minecraft world
+//     * @param user player invoking the right click action
+//     * @param hand the hand of the user
+//     * @return TypedActionResult, indicates if the use of the item succeeded or not
+//     */
+//    @Override
+//    public TypedActionResult<ItemStack> use(World world, PlayerEntity user, Hand hand) {
+//        ItemStack itemStack = user.getStackInHand(hand); // creates a new ItemStack instance of the user's itemStack in-hand
+//
+//        /* TODO sound effect of Neutron throw
+//         * Example with snowball sound
+//         * world.playSound(null, user.getX(), user.getY(), user.getZ(), SoundEvents.ENTITY_SNOWBALL_THROW, SoundCategory.NEUTRAL, 0.5F, 1F);
+//         */
+//
+//        /* TODO cooldown on the throw of an neutron (like the cooldown on Ender Pearls)
+//         * Example cooldown of 5 ticks
+//         * user.getItemCooldownManager().set(this, 5);
+//         */
+//        if (!world.isClient) {
+//            // Spawns the neutron entity with correct initial velocity (velocity has the same direction as the players looking direction)
+//            NeutronEntity neutronEntity = new NeutronEntity(world, user);
+//            neutronEntity.setItem(itemStack);
+//            neutronEntity.setVelocity(user, user.getPitch(), user.getYaw(), user.getRoll(), 1.5F, 0F);
+//            world.spawnEntity(neutronEntity);
+//        }
+//
+//        user.incrementStat(Stats.USED.getOrCreateStat(this));
+//        if (!user.getAbilities().creativeMode) {
+//            itemStack.decrement(1); // decrements itemStack if user is not in creative mode
+//        }
+//
+//        return TypedActionResult.success(itemStack, world.isClient());
+//    }
 }
