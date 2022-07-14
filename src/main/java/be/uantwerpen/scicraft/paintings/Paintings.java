a96e873fc1a620d81472c1283a45380f65acb92b
pixar02 <pixar.pf@gmail.com>
2022-07-14 14:51:11 +0200

    fixs Paintings

--- src/main/java/be/uantwerpen/scicraft/paintings/Paintings.java
+++ src/main/java/be/uantwerpen/scicraft/paintings/Paintings.java
@@ -1,24 +1,24 @@
 package be.uantwerpen.scicraft.paintings;
 
 import be.uantwerpen.scicraft.Scicraft;
-import net.minecraft.entity.decoration.painting.PaintingMotive;
+import net.minecraft.entity.decoration.painting.PaintingVariant;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
 
 public class Paintings {
 
-    public static final PaintingMotive PARTICLE_CPY = register(new PaintingMotive(32, 32), "particle_cpy");
-    public static final PaintingMotive PARTICLE_RGB = register(new PaintingMotive(32, 32), "particle_rgb");
+    public static final PaintingVariant PARTICLE_CPY = register(new PaintingVariant(32, 32), "particle_cpy");
+    public static final PaintingVariant PARTICLE_RGB = register(new PaintingVariant(32, 32), "particle_rgb");
 
     /**
      * Register a new painting
      *
      * @param motive : new painting
      * @param id     : string name of painting
-     * @return {@link PaintingMotive}
+     * @return {@link PaintingVariant}
      */
-    private static PaintingMotive register(PaintingMotive motive, String id) {
-        return Registry.register(Registry.PAINTING_MOTIVE, new Identifier(Scicraft.MOD_ID, id), motive);
+    private static PaintingVariant register(PaintingVariant motive, String id) {
+        return Registry.register(Registry.PAINTING_VARIANT, new Identifier(Scicraft.MOD_ID, id), motive);
     }
 
     public static void registerPaintings() {
