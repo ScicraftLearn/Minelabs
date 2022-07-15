9282a29ce4a697ff373122514f0dcf8b2e4835a3
Joey De Pauw <joeydepauw@gmail.com>
2022-07-15 22:14:34 +0200

    add interface to vertices and edges

--- src/main/java/be/uantwerpen/scicraft/util/Graph.java
+++ src/main/java/be/uantwerpen/scicraft/util/Graph.java
@@ -1,6 +1,7 @@
 package be.uantwerpen.scicraft.util;
 
 import java.util.*;
+import java.util.stream.Collectors;
 
 /**
  * Basic implementation of undirected graph.
@@ -14,36 +15,46 @@ public class Graph<V, E> {
     public class Vertex {
         public V data;
 
+        private final Set<Edge> edges = new HashSet<>();
+
         private Vertex(V data) {
             this.data = data;
+        }
+
+        public Collection<Vertex> getNeighbours(){
+            return edges.stream().flatMap(edge -> edge.getVertices().stream().filter(vertex -> vertex != this)).collect(Collectors.toSet());
+        }
+
+        public Collection<V> getNeighboursData(){
+            return getNeighbours().stream().map(vertex -> vertex.data).collect(Collectors.toSet());
+        }
+
+        public Collection<Edge> getEdges(){
+            return edges;
+        }
+
+        public Collection<E> getEdgesData(){
+            return edges.stream().map(edge -> edge.data).collect(Collectors.toSet());
         }
     }
 
     public class Edge {
-        public final Vertex v1;
-        public final Vertex v2;
+        private final Set<Vertex> vertices;
         public E data;
 
-        protected Edge(Vertex v1, Vertex v2, E data) {
-            this.v1 = v1;
-            this.v2 = v2;
+        protected Edge(Set<Vertex> vertices, E data) {
+            this.vertices = vertices;
             this.data = data;
+        }
+
+        public Collection<Vertex> getVertices(){
+            return vertices;
         }
     }
 
     private final Set<Vertex> vertices = new HashSet<>();
 
-    class EdgeKey{
-        Vertex v1;
-        Vertex v2;
-
-        private EdgeKey(Vertex v1, Vertex v2){
-            this.v1 = v1;
-            this.v2 = v2;
-        }
-    }
-
-    private final Map<EdgeKey, Edge> edges = new HashMap<>();
+    private final Map<Set<Vertex>, Edge> edges = new HashMap<>();
 
     public Set<Vertex> getVertices(){
         return vertices;
@@ -56,12 +67,17 @@ public class Graph<V, E> {
     }
 
     public Edge addEdge(Vertex v1, Vertex v2, E data) throws IllegalArgumentException {
-        EdgeKey edgeKey = new EdgeKey(v1, v2);
-        if (edges.containsKey(edgeKey))
+        Set<Vertex> vertices = Set.of(v1, v2);
+        if (edges.containsKey(vertices))
             throw new IllegalArgumentException("Edge already exists");
-        Edge edge = new Edge(v1, v2, data);
-        edges.put(edgeKey, edge);
-        edges.put(new EdgeKey(v2, v1), edge);       // undirected graph
+        Edge edge = new Edge(vertices, data);
+
+        // keep vertices consistent
+        v1.edges.add(edge);
+        v2.edges.add(edge);
+
+        // store in graph
+        edges.put(vertices, edge);
         return edge;
     }
 
