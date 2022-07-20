c7652770a10d442fd8a223391113500fe989d811
JorenFa <jorenfabre@gmail.com>
2022-07-21 01:29:21 +0200

    Stopped Erlenmeyers from displaying in every creative tab Also fixed their names

--- src/main/java/be/uantwerpen/scicraft/potion/GasPotion.java
+++ src/main/java/be/uantwerpen/scicraft/potion/GasPotion.java
@@ -1,11 +1,7 @@
 package be.uantwerpen.scicraft.potion;
 
-import net.minecraft.item.ItemGroup;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.LingeringPotionItem;
-import net.minecraft.potion.Potion;
-import net.minecraft.potion.PotionUtil;
-import net.minecraft.util.collection.DefaultedList;
 
 public class GasPotion extends LingeringPotionItem {
     public GasPotion(Settings settings) {
@@ -13,8 +9,9 @@ public class GasPotion extends LingeringPotionItem {
     }
 
     @Override
-    public void appendStacks(ItemGroup group, DefaultedList<ItemStack> stacks) {
-        Potion potion = new Potion("Test");
-        stacks.add(PotionUtil.setPotion(new ItemStack(this), potion));
+    public ItemStack getDefaultStack() {
+        ItemStack stack = new ItemStack(this);
+        stack.getOrCreateNbt().putString("Potion", "minecraft:empty");
+        return stack;
     }
 }
