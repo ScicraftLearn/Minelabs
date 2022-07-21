4dcccd4beb653e76404917b58c1c2af8469c7e88
brent <brent.budts11@gmail.com>
2022-07-21 14:10:25 +0200

    bugs mologram fixed (block is now properly saved)

--- src/main/java/be/uantwerpen/scicraft/block/entity/MologramBlockEntity.java
+++ src/main/java/be/uantwerpen/scicraft/block/entity/MologramBlockEntity.java
@@ -3,12 +3,14 @@ package be.uantwerpen.scicraft.block.entity;
 
 import net.minecraft.block.BlockState;
 import net.minecraft.block.entity.BlockEntity;
+import net.minecraft.inventory.Inventories;
 import net.minecraft.inventory.SimpleInventory;
 import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NbtCompound;
 import net.minecraft.network.Packet;
 import net.minecraft.network.listener.ClientPlayPacketListener;
 import net.minecraft.network.packet.s2c.play.BlockEntityUpdateS2CPacket;
+import net.minecraft.util.collection.DefaultedList;
 import net.minecraft.util.math.BlockPos;
 
 import javax.annotation.Nullable;
@@ -18,6 +20,7 @@ public class MologramBlockEntity extends BlockEntity {
     private int rot_x = 0;
     private int rot_y = 0;
     private int rot_z = 0;
+
     public MologramBlockEntity(BlockPos pos, BlockState state) {
         super(BlockEntities.MOLOGRAM_BLOCK_ENTITY, pos, state);
     }
@@ -31,10 +34,11 @@ public class MologramBlockEntity extends BlockEntity {
         tag.putInt("rotate_y", rot_y);
         tag.putInt("rotate_z", rot_z);
 
-        inventory.toNbtList();
+        Inventories.writeNbt(tag, DefaultedList.ofSize(1, inventory.getStack(0)));
 
         super.writeNbt(tag);
     }
+
     // Deserialize the BlockEntity
     @Override
     public void readNbt(NbtCompound tag) {
@@ -44,8 +48,9 @@ public class MologramBlockEntity extends BlockEntity {
         rot_y = tag.getInt("rotate_y");
         rot_z = tag.getInt("rotate_z");
 
-        inventory.readNbtList(inventory.toNbtList());
-
+        DefaultedList<ItemStack> stacks = DefaultedList.ofSize(1,ItemStack.EMPTY);
+        Inventories.readNbt(tag, stacks);
+        inventory.setStack(0,stacks.get(0));
     }
 
     //sync data server client:
