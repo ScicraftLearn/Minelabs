09b5f545cc14a128b817ca6bc74c2a66d6b54ddf
Elias Neel <eliasneel2001@gmail.com>
2022-07-25 13:33:54 +0200

    atom click is working

--- src/main/java/be/uantwerpen/scicraft/block/PortalBlock.java
+++ src/main/java/be/uantwerpen/scicraft/block/PortalBlock.java
@@ -1,6 +1,7 @@
 package be.uantwerpen.scicraft.block;
 
 import be.uantwerpen.scicraft.dimension.ModDimensions;
+import be.uantwerpen.scicraft.item.ItemGroups;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
 import net.minecraft.entity.player.PlayerEntity;
@@ -15,56 +16,65 @@ import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Position;
 import net.minecraft.world.*;
 
+
 public class PortalBlock extends Block {
     public PortalBlock(Settings settings) {
         super(settings);
     }
+
     private Position playerpos = null;
+
+
     @Override
     public ActionResult onUse(BlockState state, World world, BlockPos pos,
                               PlayerEntity player, Hand hand, BlockHitResult hit) {
-
         if (!world.isClient) {
             if (!player.isSneaking()) {
+                //Get world instance
                 MinecraftServer server = world.getServer();
                 if (server != null) {
                     if (player instanceof ServerPlayerEntity) {
                         ServerPlayerEntity serverPlayer = (ServerPlayerEntity) player;
-                        //If player is in subatomic dimension
-                        if (world.getRegistryKey() == ModDimensions.SUBATOM_KEY) {
-                            ServerWorld overWorld = server.getWorld(World.OVERWORLD);
-                            if (overWorld != null) {
-                                if(playerpos!=null){
-                                    serverPlayer.teleport(overWorld, playerpos.getX(), playerpos.getY(), playerpos.getZ(),
-                                            serverPlayer.bodyYaw, serverPlayer.prevPitch);
-                                }else{
-                                    playerpos=player.getPos();
-                                    serverPlayer.teleport(overWorld, playerpos.getX(), overWorld.getTopY(), playerpos.getZ(),
-                                            serverPlayer.bodyYaw, serverPlayer.prevPitch);
-                                }
-                                playerpos=null;
-                                //BlockPos destPos = getDest(player.getBlockPos(), overWorld, false);
-                            }
-                        }
-                        //If player is in overworld
-                        else {
-                            //Save player position so it can be returned
-                            playerpos=player.getPos();
-                            ServerWorld atomdim = server.getWorld(ModDimensions.SUBATOM_KEY);
-                            if (atomdim != null) {
-                                BlockPos destPos = getDest(serverPlayer.getBlockPos(), atomdim, true);
-                                boolean doSetBlock = true;
-                                for (BlockPos checkPos : BlockPos.iterate(destPos.down(10).west(10).south(10), destPos.up(10).east(10).north(10))) {
-                                    if (atomdim.getBlockState(checkPos).getBlock() == Blocks.ATOM_PORTAL) {
-                                        doSetBlock = false;
-                                        break;
+
+
+                        //Only teleport if an atom is used to right click
+                        if(player.getStackInHand(hand).getItem().getGroup()==ItemGroups.ATOMS){
+                            //If player is in subatomic dimension
+                            if (world.getRegistryKey() == ModDimensions.SUBATOM_KEY) {
+                                ServerWorld overWorld = server.getWorld(World.OVERWORLD);
+                                if (overWorld != null) {
+                                    if (playerpos != null) {
+                                        serverPlayer.teleport(overWorld, playerpos.getX(), playerpos.getY(), playerpos.getZ(),
+                                                serverPlayer.bodyYaw, serverPlayer.prevPitch);
+                                    } else {
+                                        playerpos = player.getPos();
+                                        serverPlayer.teleport(overWorld, playerpos.getX(), overWorld.getTopY(), playerpos.getZ(),
+                                                serverPlayer.bodyYaw, serverPlayer.prevPitch);
                                     }
+                                    playerpos = null;
+                                    //BlockPos destPos = getDest(player.getBlockPos(), overWorld, false);
                                 }
-                                if (doSetBlock) {
-                                    atomdim.setBlockState(destPos, Blocks.ATOM_PORTAL.getDefaultState());
+                            }
+                            //If player is in overworld
+                            else {
+                                //Save player position so it can be returned
+                                playerpos = player.getPos();
+                                ServerWorld atomdim = server.getWorld(ModDimensions.SUBATOM_KEY);
+                                if (atomdim != null) {
+                                    BlockPos destPos = getDest(serverPlayer.getBlockPos(), atomdim, true);
+                                    boolean doSetBlock = true;
+                                    for (BlockPos checkPos : BlockPos.iterate(destPos.down(10).west(10).south(10), destPos.up(10).east(10).north(10))) {
+                                        if (atomdim.getBlockState(checkPos).getBlock() == Blocks.ATOM_PORTAL) {
+                                            doSetBlock = false;
+                                            break;
+                                        }
+                                    }
+                                    if (doSetBlock) {
+                                        atomdim.setBlockState(destPos, Blocks.ATOM_PORTAL.getDefaultState());
+                                    }
+                                    serverPlayer.teleport(atomdim, 0, -63, 0,
+                                            serverPlayer.bodyYaw, serverPlayer.prevPitch);
                                 }
-                                serverPlayer.teleport(atomdim, 0, -63, 0,
-                                        serverPlayer.bodyYaw, serverPlayer.prevPitch);
                             }
                         }
                         return ActionResult.SUCCESS;
