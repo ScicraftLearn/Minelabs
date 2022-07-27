478c86d4a65d4bb9c32cc8499c2c325833fd6654
Elias Neel <eliasneel2001@gmail.com>
2022-07-27 13:37:57 +0200

    dimensie checking + item checking funtioning correctly

--- src/main/java/be/uantwerpen/scicraft/event/ModEvents.java
+++ src/main/java/be/uantwerpen/scicraft/event/ModEvents.java
@@ -1,6 +1,7 @@
 package be.uantwerpen.scicraft.event;
 
 import be.uantwerpen.scicraft.dimension.ModDimensions;
+import be.uantwerpen.scicraft.item.ItemGroups;
 import net.fabricmc.fabric.api.event.player.UseItemCallback;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.TypedActionResult;
@@ -10,12 +11,19 @@ public class ModEvents {
     public static void registerEvents() {
         UseItemCallback.EVENT.register((player, world, hand) ->
                 {
-                    if (world.getDimensionKey().equals(ModDimensions.SUBATOM_KEY)) {
-                        System.out.println("ja!");
-                        return TypedActionResult.success(player.getStackInHand(hand));
+                    if (world.getRegistryKey()==(ModDimensions.SUBATOM_KEY)) {
+                        ItemStack stack=player.getStackInHand(hand);
+                        if(stack.getItem().getGroup()== ItemGroups.ATOMS){
+                            System.out.println("ja!");
+                            return TypedActionResult.success(player.getStackInHand(hand));
+                        }
+                        else{
+                            System.out.println("neen");
+                            return TypedActionResult.fail(stack);
+                        }
                     } else {
-                        System.out.println("nee");
-                        return TypedActionResult.fail(ItemStack.EMPTY);
+                        System.out.println("andere dimensie");
+                        return TypedActionResult.success(player.getStackInHand(hand));
                     }
                 }
         );
