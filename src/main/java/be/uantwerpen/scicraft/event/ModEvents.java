d66c68aebf65970feb3ccf7db805f36f6f1f4c1d
Elias Neel <eliasneel2001@gmail.com>
2022-07-27 13:27:11 +0200

    event

--- src/main/java/be/uantwerpen/scicraft/event/ModEvents.java
+++ src/main/java/be/uantwerpen/scicraft/event/ModEvents.java
@@ -1,21 +1,20 @@
 package be.uantwerpen.scicraft.event;
 
-import be.uantwerpen.scicraft.item.ItemGroups;
+import be.uantwerpen.scicraft.dimension.ModDimensions;
 import net.fabricmc.fabric.api.event.player.UseItemCallback;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.TypedActionResult;
 
+//Klasse werkt niet
 public class ModEvents {
     public static void registerEvents() {
-        UseItemCallback.EVENT.register((player, item, hand) ->
+        UseItemCallback.EVENT.register((player, world, hand) ->
                 {
-                    //Checks to prevent null errors
-                    ItemStack stack = player.getStackInHand(hand);
-                    if (stack.getItem().getGroup() == ItemGroups.ATOMS) {
-                        System.out.println("Atom baby!");
-                        return TypedActionResult.consume(stack);
+                    if (world.getDimensionKey().equals(ModDimensions.SUBATOM_KEY)) {
+                        System.out.println("ja!");
+                        return TypedActionResult.success(player.getStackInHand(hand));
                     } else {
-                        System.out.println("No blocks");
+                        System.out.println("nee");
                         return TypedActionResult.fail(ItemStack.EMPTY);
                     }
                 }
