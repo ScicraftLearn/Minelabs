febc1efc3b3fea54c663bdf493ba8a38375dce5e
Elias Neel <eliasneel2001@gmail.com>
2022-07-27 14:37:30 +0200

    no more placing blocks in the atomic dimension!

--- src/main/java/be/uantwerpen/scicraft/event/ModEvents.java
+++ src/main/java/be/uantwerpen/scicraft/event/ModEvents.java
@@ -2,36 +2,23 @@ package be.uantwerpen.scicraft.event;
 
 import be.uantwerpen.scicraft.dimension.ModDimensions;
 import be.uantwerpen.scicraft.item.ItemGroups;
-import net.fabricmc.fabric.api.event.player.UseItemCallback;
-import net.minecraft.block.Block;
-import net.minecraft.entity.player.PlayerEntity;
-import net.minecraft.item.ItemGroup;
-import net.minecraft.item.ItemStack;
-import net.minecraft.util.TypedActionResult;
+import be.uantwerpen.scicraft.item.Items;
+import net.fabricmc.fabric.api.event.player.UseBlockCallback;
+import net.minecraft.util.ActionResult;
 
 //Klasse werkt niet
 public class ModEvents {
     public static void registerEvents() {
-        UseItemCallback.EVENT.register((player, world, hand) ->
-                {
-                    if (world.getRegistryKey()==(ModDimensions.SUBATOM_KEY)) {
-                        ItemStack stack=player.getStackInHand(hand);
-                        if(stack.getItem().getGroup()== ItemGroups.ATOMS){
-                            System.out.println("ja!");
-                            return TypedActionResult.success(player.getStackInHand(hand));
-                        }
-                        else{
-                            if(stack.getItem().getGroup()== ItemGroup.BUILDING_BLOCKS){
-                                
-                            }
-                            System.out.println("neen");
-                            return TypedActionResult.fail(stack);
-                        }
-                    } else {
-                        System.out.println("andere dimensie");
-                        return TypedActionResult.success(player.getStackInHand(hand));
-                    }
+        UseBlockCallback.EVENT.register((player,world,hand,block)->{
+            if(world.getRegistryKey()==ModDimensions.SUBATOM_KEY){
+                if(player.getStackInHand(hand).getItem().getGroup()==ItemGroups.ATOMS || player.getStackInHand(hand).isOf(Items.ATOM_PORTAL)){
+                    return ActionResult.PASS;
+                }else {
+                    return ActionResult.FAIL;
                 }
-        );
+            }else{
+                return ActionResult.PASS;
+            }
+        });
     }
 }
