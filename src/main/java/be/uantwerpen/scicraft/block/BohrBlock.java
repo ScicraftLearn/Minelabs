f230858e3a83c1c208a5713e4fa4ab0535879a6a
brent <brent.budts11@gmail.com>
2022-07-28 17:08:56 +0200

    neutron inventory

--- src/main/java/be/uantwerpen/scicraft/block/BohrBlock.java
+++ src/main/java/be/uantwerpen/scicraft/block/BohrBlock.java
@@ -1,9 +1,18 @@
 package be.uantwerpen.scicraft.block;
 
 import be.uantwerpen.scicraft.block.entity.BohrBlockEntity;
+import be.uantwerpen.scicraft.item.Items;
 import net.minecraft.block.*;
 import net.minecraft.block.entity.BlockEntity;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.inventory.Inventory;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.ActionResult;
+import net.minecraft.util.Hand;
 import net.minecraft.util.ItemScatterer;
+import net.minecraft.util.collection.DefaultedList;
+import net.minecraft.util.hit.BlockHitResult;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 import org.jetbrains.annotations.Nullable;
@@ -22,5 +31,32 @@ public class BohrBlock extends BlockWithEntity implements BlockEntityProvider {
     @Override
     public BlockEntity createBlockEntity(BlockPos pos, BlockState state) {
         return new BohrBlockEntity(pos,state);
+    }
+
+    @Override
+    public ActionResult onUse(BlockState state, World world, BlockPos pos, PlayerEntity player, Hand hand, BlockHitResult hit) {
+
+        BlockEntity blockEntity = world.getBlockEntity(pos);
+
+        DefaultedList<ItemStack> neutronInventory = ((BohrBlockEntity) blockEntity).getNeutronInventory();
+
+        if(!player.getStackInHand(hand).isEmpty()){
+            if(player.getStackInHand(hand).getItem()== Items.NEUTRON){
+                for(int i = 0; i<3; i++){
+                    if (neutronInventory.get(i).isEmpty()){
+                        neutronInventory.set(i,(player.getStackInHand(hand).copy()));
+                        neutronInventory.get(i).setCount(1);
+                        return ActionResult.SUCCESS;
+                    }
+                    if(neutronInventory.get(i).getCount()<64){
+                        neutronInventory.get(i).setCount(neutronInventory.get(i).getCount()+1);
+                        player.getStackInHand(hand).decrement(1);
+                        System.out.println("Slot holds : "+ neutronInventory.get(i));
+                        return ActionResult.SUCCESS;
+                    }
+                }
+            }
+        }
+        return ActionResult.SUCCESS;
     }
 }
--- src/main/java/be/uantwerpen/scicraft/block/entity/BohrBlockEntity.java
+++ src/main/java/be/uantwerpen/scicraft/block/entity/BohrBlockEntity.java
@@ -1,12 +1,56 @@
 package be.uantwerpen.scicraft.block.entity;
 
+import be.uantwerpen.scicraft.inventory.ImplementedInventory;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.entity.BlockEntity;
 import net.minecraft.block.entity.BlockEntityType;
+import net.minecraft.inventory.Inventories;
+import net.minecraft.item.ItemStack;
+import net.minecraft.nbt.NbtCompound;
+import net.minecraft.util.collection.DefaultedList;
 import net.minecraft.util.math.BlockPos;
 
-public class BohrBlockEntity extends BlockEntity{
+public class BohrBlockEntity extends BlockEntity implements ImplementedInventory {
+
+    private final DefaultedList<ItemStack> protonInventory = DefaultedList.ofSize(3, ItemStack.EMPTY);
+    private final DefaultedList<ItemStack> neutronInventory = DefaultedList.ofSize(3, ItemStack.EMPTY);
+    private final DefaultedList<ItemStack> electronInventory = DefaultedList.ofSize(3, ItemStack.EMPTY);
+
     public BohrBlockEntity( BlockPos pos, BlockState state) {
         super(BlockEntities.BOHR_BLOCK_ENTITY, pos, state);
     }
+
+    @Override
+    public DefaultedList<ItemStack> getItems() {
+        return null;
+    }
+
+    public DefaultedList<ItemStack> getProtonInventory() {
+        return protonInventory;
+    }
+
+    public DefaultedList<ItemStack> getNeutronInventory() {
+        return neutronInventory;
+    }
+
+    public DefaultedList<ItemStack> getElectronInventory() {
+        return electronInventory;
+    }
+
+    @Override
+    public void readNbt(NbtCompound nbt) {
+        super.readNbt(nbt);
+        Inventories.readNbt(nbt, this.protonInventory);
+        Inventories.readNbt(nbt, this.neutronInventory);
+        Inventories.readNbt(nbt, this.electronInventory);
+    }
+
+    @Override
+    public void writeNbt(NbtCompound nbt) {
+        super.writeNbt(nbt);
+        Inventories.writeNbt(nbt, this.protonInventory);
+        Inventories.writeNbt(nbt, this.neutronInventory);
+        Inventories.writeNbt(nbt, this.electronInventory);
+    }
+
 }
