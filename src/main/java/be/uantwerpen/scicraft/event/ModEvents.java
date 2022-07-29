e078b31b1f89c5e9bf6967d14f7d68971463020e
Elias Neel <eliasneel2001@gmail.com>
2022-07-29 09:24:28 +0200

    bohr plate only placed in subatomic dimension

--- src/main/java/be/uantwerpen/scicraft/event/ModEvents.java
+++ src/main/java/be/uantwerpen/scicraft/event/ModEvents.java
@@ -5,6 +5,7 @@ import be.uantwerpen.scicraft.dimension.ModDimensions;
 import be.uantwerpen.scicraft.item.ItemGroups;
 import be.uantwerpen.scicraft.item.Items;
 import net.fabricmc.fabric.api.event.player.UseBlockCallback;
+import net.minecraft.item.Item;
 import net.minecraft.util.ActionResult;
 
 public class ModEvents {
@@ -12,7 +13,7 @@ public class ModEvents {
         //Check for dimension and block used
         UseBlockCallback.EVENT.register((player,world,hand,block)->{
             if(world.getRegistryKey()==ModDimensions.SUBATOM_KEY){
-                if(player.getStackInHand(hand).getItem().getGroup()==ItemGroups.ATOMS || player.getStackInHand(hand).isOf(Items.ATOM_PORTAL)){
+                if(player.getStackInHand(hand).getItem().getGroup()==ItemGroups.ATOMS || player.getStackInHand(hand).isOf(Items.ATOM_PORTAL) || player.getStackInHand(hand).isOf(Items.BOHR_BLOCK)){
                     return ActionResult.PASS;
                 }else {
                     return ActionResult.FAIL;
