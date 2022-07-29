d7c1820c5d6e7f694b8bb1f58e2fe1c076905d3d
Elias Neel <eliasneel2001@gmail.com>
2022-07-29 14:28:52 +0200

    spawning and despawning now linked between dimension and field

--- src/main/java/be/uantwerpen/scicraft/block/AtomicFloor.java
+++ src/main/java/be/uantwerpen/scicraft/block/AtomicFloor.java
@@ -5,7 +5,9 @@ import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.random.Random;
 import net.minecraft.world.World;
 
+
 public class AtomicFloor extends Block {
+    private static int fields=0;
 
     public AtomicFloor(Settings settings) {
         super(Settings.of(Material.BARRIER).nonOpaque());
@@ -15,36 +17,38 @@ public class AtomicFloor extends Block {
     public void randomDisplayTick(BlockState state, World world, BlockPos pos, Random random) {
         //System.out.println("random ding");
         java.util.Random r = new java.util.Random();
-        int rvalue = r.nextInt(10000);
+        int rvalue = r.nextInt(100000);
+        int rheight = r.nextInt(32);
+        pos = pos.up(rheight);
         //Only change air blocks so other fields don't get replaced
-        if (world.getBlockState(pos.up(2)) == net.minecraft.block.Blocks.AIR.getDefaultState()) {
+        if (world.getBlockState(pos) == net.minecraft.block.Blocks.AIR.getDefaultState() && (rvalue>=1500 && rvalue <1520) && fields <64) {
             if (rvalue >= 1500 && rvalue < 1502) {
                 System.out.println("Naar ELECTRON");
-                world.setBlockState(pos.up(2), Blocks.ELECTRON_QUANTUMFIELD.getDefaultState());
+                world.setBlockState(pos, Blocks.ELECTRON_QUANTUMFIELD.getDefaultState());
             } else if (rvalue >= 1502 && rvalue < 1504) {
                 System.out.println("Naar DOWNQUARK");
-                world.setBlockState(pos.up(2), Blocks.DOWNQUARK_QUANTUMFIELD.getDefaultState());
+                world.setBlockState(pos, Blocks.DOWNQUARK_QUANTUMFIELD.getDefaultState());
             } else if (rvalue >= 1504 && rvalue < 1508) {
                 System.out.println("Naar GLUON");
-                world.setBlockState(pos.up(2), Blocks.GLUON_QUANTUMFIELD.getDefaultState());
+                world.setBlockState(pos, Blocks.GLUON_QUANTUMFIELD.getDefaultState());
             } else if (rvalue >= 1508 && rvalue < 1510) {
                 System.out.println("Naar PHOTON");
-                world.setBlockState(pos.up(2), Blocks.PHOTON_QUANTUMFIELD.getDefaultState());
+                world.setBlockState(pos, Blocks.PHOTON_QUANTUMFIELD.getDefaultState());
             } else if (rvalue >= 1510 && rvalue < 1512) {
                 System.out.println("Naar UPQUARK");
-                world.setBlockState(pos.up(2), Blocks.UPQUARK_QUANTUMFIELD.getDefaultState());
+                world.setBlockState(pos, Blocks.UPQUARK_QUANTUMFIELD.getDefaultState());
             } else if (rvalue >= 1512 && rvalue < 1514) {
                 System.out.println("Naar WEAK BOSON");
-                world.setBlockState(pos.up(2), Blocks.WEAK_BOSON_QUANTUMFIELD.getDefaultState());
+                world.setBlockState(pos, Blocks.WEAK_BOSON_QUANTUMFIELD.getDefaultState());
             } else if (rvalue >= 1514 && rvalue < 1520) {
                 System.out.println("Naar NEUTRINO");
-                world.setBlockState(pos.up(2), Blocks.NEUTRINO_QUANTUMFIELD.getDefaultState());
+                world.setBlockState(pos, Blocks.NEUTRINO_QUANTUMFIELD.getDefaultState());
             }
-        }else{
-            if (rvalue < 1500 && world.getBlockState(pos.up(2)) != net.minecraft.block.Blocks.AIR.getDefaultState()) {
-                System.out.println("Naar air!");
-                world.setBlockState(pos.up(2), net.minecraft.block.Blocks.AIR.getDefaultState());
-            }
+            fields+=1;
+            System.out.println("fields:"+fields);
         }
+    }
+    public static void decreaseFields(){
+        fields-=1;
     }
 }
--- src/main/java/be/uantwerpen/scicraft/block/QuantumfieldBlock.java
+++ src/main/java/be/uantwerpen/scicraft/block/QuantumfieldBlock.java
@@ -1,18 +1,26 @@
 package be.uantwerpen.scicraft.block;
 
+import be.uantwerpen.scicraft.dimension.ModDimensions;
 import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
+import net.minecraft.block.Blocks;
 import net.minecraft.block.Material;
 import net.minecraft.block.entity.BlockEntity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.ItemStack;
+import net.minecraft.server.world.ServerWorld;
 import net.minecraft.stat.Stats;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.random.Random;
 import net.minecraft.world.World;
 import org.jetbrains.annotations.Nullable;
 
 public class QuantumfieldBlock extends Block {
+    //Ticklife for subatomic dimension
+    java.util.Random r = new java.util.Random();
+    private int tickLife = r.nextInt(6000);
+
     public QuantumfieldBlock() {
         // Properties of all quantumfield blocks
         // Change the first value in strength to get the wanted mining speed
@@ -23,5 +31,17 @@ public class QuantumfieldBlock extends Block {
     public void afterBreak(World world, PlayerEntity player, BlockPos pos, BlockState state, @Nullable BlockEntity blockEntity, ItemStack stack) {
         super.afterBreak(world, player, pos, state, blockEntity, stack);
         world.setBlockState(pos, state);
+    }
+
+    @Override
+    public void randomDisplayTick(BlockState state, World world, BlockPos pos, Random random) {
+        int destruction=r.nextInt(20);
+        tickLife -= destruction;
+        //System.out.println("leventje weg");
+        if (tickLife <= 0) {
+            world.setBlockState(pos, Blocks.AIR.getDefaultState());
+            //System.out.println("field dood");
+            AtomicFloor.decreaseFields();
+        }
     }
 }
