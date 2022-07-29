1734a1fa8db700cee7487d1d71b235f8779c9020
Elias Neel <eliasneel2001@gmail.com>
2022-07-29 12:19:32 +0200

    quantumfield spawn and despawn within minecraft logic distance distance

--- src/main/java/be/uantwerpen/scicraft/block/AtomicFloor.java
+++ src/main/java/be/uantwerpen/scicraft/block/AtomicFloor.java
@@ -1,12 +1,11 @@
 package be.uantwerpen.scicraft.block;
 
 import net.minecraft.block.*;
-import net.minecraft.server.MinecraftServer;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.random.Random;
 import net.minecraft.world.World;
 
-public class AtomicFloor extends Block{
+public class AtomicFloor extends Block {
 
     public AtomicFloor(Settings settings) {
         super(Settings.of(Material.BARRIER).nonOpaque());
@@ -15,10 +14,37 @@ public class AtomicFloor extends Block{
     @Override
     public void randomDisplayTick(BlockState state, World world, BlockPos pos, Random random) {
         //System.out.println("random ding");
-        java.util.Random r=new java.util.Random();
-        if(r.nextInt(100000)>99990){
-            System.out.println("50 procent?");
-            world.setBlockState(pos.up(2),Blocks.ELECTRON_QUANTUMFIELD.getDefaultState());
+        java.util.Random r = new java.util.Random();
+        int rvalue = r.nextInt(10000);
+        //Only change air blocks so other fields don't get replaced
+        if (world.getBlockState(pos.up(2)) == net.minecraft.block.Blocks.AIR.getDefaultState()) {
+            if (rvalue >= 1500 && rvalue < 1502) {
+                System.out.println("Naar ELECTRON");
+                world.setBlockState(pos.up(2), Blocks.ELECTRON_QUANTUMFIELD.getDefaultState());
+            } else if (rvalue >= 1502 && rvalue < 1504) {
+                System.out.println("Naar DOWNQUARK");
+                world.setBlockState(pos.up(2), Blocks.DOWNQUARK_QUANTUMFIELD.getDefaultState());
+            } else if (rvalue >= 1504 && rvalue < 1508) {
+                System.out.println("Naar GLUON");
+                world.setBlockState(pos.up(2), Blocks.GLUON_QUANTUMFIELD.getDefaultState());
+            } else if (rvalue >= 1508 && rvalue < 1510) {
+                System.out.println("Naar PHOTON");
+                world.setBlockState(pos.up(2), Blocks.PHOTON_QUANTUMFIELD.getDefaultState());
+            } else if (rvalue >= 1510 && rvalue < 1512) {
+                System.out.println("Naar UPQUARK");
+                world.setBlockState(pos.up(2), Blocks.UPQUARK_QUANTUMFIELD.getDefaultState());
+            } else if (rvalue >= 1512 && rvalue < 1514) {
+                System.out.println("Naar WEAK BOSON");
+                world.setBlockState(pos.up(2), Blocks.WEAK_BOSON_QUANTUMFIELD.getDefaultState());
+            } else if (rvalue >= 1514 && rvalue < 1520) {
+                System.out.println("Naar NEUTRINO");
+                world.setBlockState(pos.up(2), Blocks.NEUTRINO_QUANTUMFIELD.getDefaultState());
+            }
+        }else{
+            if (rvalue < 1500 && world.getBlockState(pos.up(2)) != net.minecraft.block.Blocks.AIR.getDefaultState()) {
+                System.out.println("Naar air!");
+                world.setBlockState(pos.up(2), net.minecraft.block.Blocks.AIR.getDefaultState());
+            }
         }
     }
 }
