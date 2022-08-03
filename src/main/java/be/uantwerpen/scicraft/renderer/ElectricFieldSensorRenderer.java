ea2db9fa4a630e524b3ecf94dc742fa3661c8c4d
Joey De Pauw <joeydepauw@gmail.com>
2022-08-03 17:22:24 +0200

    fix arrow orientation

--- src/main/java/be/uantwerpen/scicraft/renderer/ElectricFieldSensorRenderer.java
+++ src/main/java/be/uantwerpen/scicraft/renderer/ElectricFieldSensorRenderer.java
@@ -1,5 +1,6 @@
 package be.uantwerpen.scicraft.renderer;
 
+import be.uantwerpen.scicraft.Scicraft;
 import be.uantwerpen.scicraft.block.entity.ElectricFieldSensorBlockEntity;
 import be.uantwerpen.scicraft.item.Items;
 import net.minecraft.client.MinecraftClient;
@@ -25,44 +26,48 @@ public class ElectricFieldSensorRenderer implements BlockEntityRenderer<Electric
     @Override
     public void render(ElectricFieldSensorBlockEntity entity, float tickDelta, MatrixStack matrices, VertexConsumerProvider vertexConsumers, int light, int overlay) {
         ItemRenderer itemRenderer = MinecraftClient.getInstance().getItemRenderer();
-        Vec3f field = entity.getField();
+        Vec3f field = entity.getField().copy();
         ItemStack stack = new ItemStack(Items.ELECTRON);
         ItemStack arrow = new ItemStack(net.minecraft.item.Items.ARROW);
 
-        // first make sure the settings are correct for the electron field
         matrices.push();
+
+        // Center arrow in block
         matrices.translate(0.5, 0.5, 0.5);
 
-        // now render the electron with the field
-        Vec3f x_rotation = new Vec3f(0, 0, -1);
-        matrices.multiply(new Quaternion(x_rotation, 45.f, true));
+        // We want the arrows to point the other direction, so we turn the field vector around.
+        field.scale(-1);
 
-        if(!field.equals(Vec3f.ZERO)) {
+        if(field.equals(Vec3f.ZERO)) {
+            // default field should be north iso east.
+            // positive x is east, so we want to rotate -90 degrees along the y-axis.
+            matrices.multiply(Direction.UP.getUnitVector().getDegreesQuaternion(90));
+        }else{
+            /*
+             * This algorithm determines the normal vector of the plane described by the original orientation of the arrow (v) and the target direction (field).
+             * It then rotates around this vector with the angle theta between the two vectors to point the arrow in the direction of the field.
+             */
+            // By default the arrow points in positive x (EAST)
             Vec3f v = new Vec3f(1,0,0);
+
+            // Compute theta with cosine formula.
             double theta = Math.acos(v.dot(field) / Math.sqrt(Math.pow(field.getX(), 2) + Math.pow(field.getY(), 2) + Math.pow(field.getZ(), 2)));
 
             if(theta == 0 || theta == Math.PI) {
-                //System.out.println(theta);
+                // When the two vectors are parallel, their cross product does not produce the normal vector of the plane.
+                // Instead we set in to one of the infinite valid normal vectors: positive Y.
                 v = Direction.UP.getUnitVector();
             } else {
                 v.cross(field);
                 v.normalize();
             }
 
-            Quaternion q = v.getRadialQuaternion((float)theta);
-            matrices.multiply(q);
+            matrices.multiply(v.getRadialQuaternion((float)theta));
         }
-        //Quaternion quaternion = new Quaternion(field.getX(), field.getY(), field.getZ(), 0);
-        //matrices.multiply(Quaternion.fromEulerXyz(quaternion.toEulerXyz()));
 
-//        Vec3f x = new Vec3f(1,0,0);
-//        Vec3f y = new Vec3f(0,1,0);
-//        Vec3f z = new Vec3f(0,0,1);
-//        double x_angle = Math.acos((x.dot(field)) / (Math.sqrt(Math.pow(x.getX(), 2) + Math.pow(x.getY(), 2) + Math.pow(x.getZ(), 2)) * Math.sqrt(Math.pow(field.getX(), 2) + Math.pow(field.getY(), 2) + Math.pow(field.getZ(), 2))));
-//        double y_angle = Math.acos((y.dot(field)) / (Math.sqrt(Math.pow(y.getX(), 2) + Math.pow(y.getY(), 2) + Math.pow(y.getZ(), 2)) * Math.sqrt(Math.pow(field.getX(), 2) + Math.pow(field.getY(), 2) + Math.pow(field.getZ(), 2))));
-//        double z_angle = Math.acos((z.dot(field)) / (Math.sqrt(Math.pow(z.getX(), 2) + Math.pow(z.getY(), 2) + Math.pow(z.getZ(), 2)) * Math.sqrt(Math.pow(field.getX(), 2) + Math.pow(field.getY(), 2) + Math.pow(field.getZ(), 2))));
-//        Vec3f euler = new Vec3f((float) x_angle * 57.2957795f, (float) y_angle * 57.2957795f, (float) z_angle * 57.2957795f);
-//        matrices.multiply(Quaternion.fromEulerXyzDegrees(euler));
+        // correct for default arrow inclination of 45 degrees.
+        Vec3f x_rotation = new Vec3f(0, 0, 1);
+        matrices.multiply(x_rotation.getDegreesQuaternion(-45));
 
         itemRenderer.renderItem(arrow, ModelTransformation.Mode.GUI,light,overlay,matrices,vertexConsumers,0);
 
