7a70d959b2dba03b4885a9e6dda00898cbb7461b
Maksim Karnaukh <s0201604@ua.ad.ac.be>
2022-08-04 17:40:30 +0200

    anti-deeltje proberen toe te voegen (anti-proton)

--- src/main/java/be/uantwerpen/scicraft/event/ModEvents.java
+++ src/main/java/be/uantwerpen/scicraft/event/ModEvents.java
@@ -28,7 +28,7 @@ public class ModEvents {
             if (world.getRegistryKey() == ModDimensions.SUBATOM_KEY) {
                 if (stack.getItem().getGroup() == ItemGroups.ATOMS || stack.isOf(Items.ATOM_PORTAL)
                         || stack.isOf(Items.BOHR_BLOCK) || stack.isOf(Items.ATOM_FLOOR) || stack.isEmpty()
-                        || stack.isOf(Items.NEUTRON) || stack.isOf(Items.PROTON) || stack.isOf(Items.ELECTRON)) {
+                        || stack.isOf(Items.NEUTRON) || stack.isOf(Items.PROTON) || stack.isOf(Items.ELECTRON) || stack.isOf(Items.ANTI_PROTON)) {
                     return ActionResult.PASS;
                 } else {
                     return ActionResult.FAIL;
