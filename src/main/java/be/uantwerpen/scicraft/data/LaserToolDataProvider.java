a9d8590de9fa188ea626ff562ef97a62cd3e789e
thdaele <thomas.vandaele2000@gmail.com>
2022-08-10 17:46:22 +0200

    expansion of blocks

--- src/main/java/be/uantwerpen/scicraft/data/LaserToolDataProvider.java
+++ src/main/java/be/uantwerpen/scicraft/data/LaserToolDataProvider.java
@@ -3,22 +3,25 @@ package be.uantwerpen.scicraft.data;
 import be.uantwerpen.scicraft.Scicraft;
 import com.google.common.collect.Sets;
 import com.google.gson.*;
+import com.google.gson.reflect.TypeToken;
 import com.google.gson.stream.JsonReader;
 import net.minecraft.data.DataGenerator;
 import net.minecraft.data.DataProvider;
 import net.minecraft.data.DataWriter;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.JsonHelper;
+import net.minecraft.util.registry.Registry;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 import java.io.FileReader;
 import java.io.IOException;
+import java.lang.reflect.Type;
 import java.nio.file.Path;
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Map;
+import java.util.*;
 import java.util.function.Consumer;
+
+//TODO mining unmineable blocks like bedrock might be problematic, same for the liquids in the list
 
 public class LaserToolDataProvider implements DataProvider {
 	private static final Logger LOGGER = LogManager.getLogger();
@@ -26,6 +29,12 @@ public class LaserToolDataProvider implements DataProvider {
 	private final DataGenerator root;
 
 	public static HashMap<String, Map<String, Integer>> moleculeNames = new HashMap<>();
+
+	private static List<String> expand = new ArrayList<>();
+	private static List<String> expandColor = new ArrayList<>();
+	private static List<String> expandWood = new ArrayList<>();
+	private static List<String> expandLeaves = new ArrayList<>();
+	private static List<String> expandCoral = new ArrayList<>();
 	
 	public LaserToolDataProvider(DataGenerator root) {
 		this.root = root;
@@ -36,7 +45,6 @@ public class LaserToolDataProvider implements DataProvider {
 		Path path = this.root.getOutput();
 		HashSet<Identifier> set = Sets.newHashSet();
 		LaserToolDataProvider.generateMinecraftBlocksData(provider -> {
-			// TODO check if block exist ingame and otherwhise try to expand the list
 			if (!set.add(provider.getId())) {
 				throw new IllegalStateException("Duplicate block " + provider.getId());
 			}
@@ -84,9 +92,11 @@ public class LaserToolDataProvider implements DataProvider {
 			// Input JSON can't be in generated directory bc this one get reset everytime datagen runs
 			input = input.getParent();
 			JsonReader reader = new JsonReader(new FileReader(input.resolve("lasertool.json").toFile()));
-			JsonArray json = GSON.fromJson(reader, JsonArray.class);
+			JsonObject json = GSON.fromJson(reader, JsonObject.class);
 
-			for (JsonElement jsonElement: json) {
+			readExpandArrays(json);
+			JsonArray dataArray = JsonHelper.getArray(json, "data");
+			for (JsonElement jsonElement: dataArray) {
 				JsonArray blocks = JsonHelper.getArray(jsonElement.getAsJsonObject(), "blocks");
 				JsonArray molecules = JsonHelper.getArray(jsonElement.getAsJsonObject(), "molecules");
 				JsonArray distribution = JsonHelper.getArray(jsonElement.getAsJsonObject(), "distribution");
@@ -94,7 +104,10 @@ public class LaserToolDataProvider implements DataProvider {
 					throw new IllegalStateException("Couldn't parse molecule in " + blocks.toString() + " molecule count and distribution does not match");
 				}
 				for (JsonElement element: blocks) {
-					data.accept(new MinecraftBlocksData(element.getAsString(), molecules, distribution));
+					List<String> expandedBlocks = expandBlock(element.getAsString());
+					for (String block: expandedBlocks) {
+						data.accept(new MinecraftBlocksData(block, molecules, distribution));
+					}
 				}
 			}
 
@@ -103,7 +116,69 @@ public class LaserToolDataProvider implements DataProvider {
 		}
 	}
 
-	public static void generateMoleculeData(Consumer<MoleculeData> data) {
+	private static List<String> expandBlock(String name) {
+		List<String> result = new ArrayList<>();
+		// Add the standard block to the list if it exists ingame
+		if (!Registry.BLOCK.get(new Identifier("minecraft", name)).equals(Registry.BLOCK.get(Registry.BLOCK.getDefaultId()))) {
+			result.add(name);
+		}
+		// For bricks and tiles expansion we need to cutoff the s at the end before we expand them
+		if (name.endsWith("_bricks") || name.endsWith("_tiles")) {
+			name = name.substring(0, name.length() - 1);
+		}
+		List<String> blocks = new ArrayList<>();
+		if (expand.contains(name)) {
+			for (String fix: new String[] {"_slab", "_stairs", "_wall", "_block", "_pillar"}) {
+				blocks.add(name + fix);
+			}
+		} else if (expandColor.contains(name)) {
+			for (String fix: new String[] {"white_", "orange_", "magenta_", "light_blue_", "yellow_", "lime_", "pink_", "gray_", "light_gray_", "cyan_", "purple_", "blue_", "brown_", "green_", "red_", "black_"}) {
+				blocks.add(fix + name);
+			}
+		} else if (expandWood.contains(name)) {
+			for (String fix: new String[] {"_log", "_planks", "_slab", "_stairs", "_wood", "_stem", "_hyphae"}) {
+				blocks.add(name + fix);
+			}
+			blocks.add("stripped_" + name + "_log");
+			blocks.add("stripped_" + name + "_wood");
+			blocks.add("stripped_" + name + "_stem");
+			blocks.add("stripped_" + name + "_hyphae");
+		} else if (expandLeaves.contains(name)) {
+			for (String fix: new String[] {"oak_", "spruce_", "birch_", "jungle_", "acacia_", "dark_oak_", "mangrove_", "azalea_", "flowering_azalea_"}) {
+				blocks.add(fix + name);
+			}
+		} else if (expandCoral.contains(name)) {
+			String[] expansion = new String[] {"tube_", "brain_", "bubble_", "fire_", "horn_"};
+			if (name.equals("dead_coral")) {
+				for (String fix: expansion) {
+					blocks.add("dead_" + fix + "coral");
+				}
+			} else {
+				for (String fix: expansion) {
+					blocks.add(fix + name);
+				}
+			}
+		} else {
+			return Collections.singletonList(name);
+		}
+		for (String block: blocks) {
+			if (!Registry.BLOCK.get(new Identifier("minecraft", block)).equals(Registry.BLOCK.get(Registry.BLOCK.getDefaultId()))) {
+				result.add(block);
+			}
+		}
+		return result;
+	}
+
+	private static void readExpandArrays(JsonObject json) {
+		Type listType = new TypeToken<List<String>>() {}.getType();
+		expand = GSON.fromJson(json.get("expand"), listType);
+		expandColor = GSON.fromJson(json.get("expand_color"), listType);
+		expandWood = GSON.fromJson(json.get("expand_wood"), listType);
+		expandLeaves = GSON.fromJson(json.get("expand_leaves"), listType);
+		expandCoral = GSON.fromJson(json.get("expand_coral"), listType);
+	}
+
+	private static void generateMoleculeData(Consumer<MoleculeData> data) {
 		for (Map.Entry<String, Map<String, Integer>> entry : moleculeNames.entrySet()) {
 			data.accept(new MoleculeData(entry.getKey(), entry.getValue()));
 		}
