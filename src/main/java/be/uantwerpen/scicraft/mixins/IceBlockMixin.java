541897dd4c06c4004c6387f32e40a5cee1ed45fc
thdaele <thomas.vandaele2000@gmail.com>
2022-08-12 17:10:09 +0200

    fixes #284

---
+++ src/main/java/be/uantwerpen/scicraft/mixins/IceBlockMixin.java
@@ -0,0 +1,24 @@
+package be.uantwerpen.scicraft.mixins;
+
+import be.uantwerpen.scicraft.util.Tags;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.IceBlock;
+import net.minecraft.block.entity.BlockEntity;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(IceBlock.class)
+public class IceBlockMixin {
+    @Inject(method = "afterBreak", at = @At(value = "INVOKE", target = "Lnet/minecraft/enchantment/EnchantmentHelper;getLevel(Lnet/minecraft/enchantment/Enchantment;Lnet/minecraft/item/ItemStack;)I"), cancellable = true)
+    public void afterBreak(World world, PlayerEntity player, BlockPos pos, BlockState state, BlockEntity blockEntity, ItemStack stack, CallbackInfo ci) {
+        if (stack.isIn(Tags.Items.LASERTOOLS)) {
+            ci.cancel();
+        }
+    }
+}
