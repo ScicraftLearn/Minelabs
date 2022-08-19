14bc8f20aac3fdae3f1e16e518953001775b4212
Sam.Roggeman@Gmail.com <Sam.Roggeman@gmail.com>
2022-08-19 18:08:18 +0200

    can see atomic floor when holding bohr plate

--- src/main/java/be/uantwerpen/scicraft/block/AtomicFloor.java
+++ src/main/java/be/uantwerpen/scicraft/block/AtomicFloor.java
@@ -1,7 +1,12 @@
 package be.uantwerpen.scicraft.block;
 
+import be.uantwerpen.scicraft.item.ItemGroups;
+import be.uantwerpen.scicraft.item.Items;
 import be.uantwerpen.scicraft.util.QuantumFieldSpawner;
 import net.minecraft.block.*;
+import net.minecraft.client.world.ClientWorld;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.BlockItem;
 import net.minecraft.server.world.ServerWorld;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.random.Random;
@@ -40,6 +45,15 @@ public class AtomicFloor extends AbstractGlassBlock {
 
     @Override
     public VoxelShape getOutlineShape(BlockState state, BlockView world, BlockPos pos, ShapeContext context) {
+        if (world instanceof ClientWorld clientWorld){
+            PlayerEntity player = clientWorld.getClosestPlayer(pos.getX(),pos.getY(),pos.getZ(),10,false);
+            if (player != null && player.getMainHandStack() != null){
+                var item = player.getMainHandStack().getItem();
+                if (item == Items.BOHR_BLOCK){
+                    return VoxelShapes.fullCube();
+                }
+            }
+        }
         return VoxelShapes.empty();
     }
 
