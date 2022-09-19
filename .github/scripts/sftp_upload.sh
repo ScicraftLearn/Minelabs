323015ce325ef77fe6ec1ac5ca5efd320a4cd91d
Bert Jorissen <info@bertjorissen.be>
2022-09-19 19:47:01 +0200

    id_rsa

--- .github/scripts/sftp_upload.sh
+++ .github/scripts/sftp_upload.sh
@@ -11,7 +11,7 @@ rm -f id_rsa id_ed id_rsa.pub id_ed.pub
 echo $PRIVATE_MINELABS_KEY >> id_rsa
 chmod 400 id_rsa
 echo $PRIVATE_MINELABS_KEY_PUB >> "id_rsa.pub"
-sftp -o "StrictHostKeyChecking no" -P 2233 -i id_rsa minelabs@minelabs.be<< EOF
+sftp -o "StrictHostKeyChecking no" -P 2233 -i ./id_rsa minelabs@minelabs.be<< EOF
 cd config
 put restart
 bye
@@ -21,7 +21,7 @@ mod_file=$(ls output| grep -E "^(minelabs-)([0-9]+)(.)([0-9]+)(.)([0-9]+)(.jar)"
 sleep 3
 echo "start start" >> restart
 
-sftp -o "StrictHostKeyChecking no" -P 2233 -i id_rsa  minelabs@minelabs.be<< EOF
+sftp -o "StrictHostKeyChecking no" -P 2233 -i ./id_rsa  minelabs@minelabs.be<< EOF
 cd minecraft-data
 rm minelabs*
 put output/$mod_file
