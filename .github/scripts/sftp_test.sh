9f1fbec2c686d3482c5c6c8557bfb21f98d81e1b
Bert Jorissen <info@bertjorissen.be>
2022-09-19 20:45:52 +0200

    env

--- .github/scripts/sftp_test.sh
+++ .github/scripts/sftp_test.sh
@@ -1,11 +1,6 @@
 #!/bin/bash
 echo "stop stop" > restart
-rm -f id_rsa id_ed id_rsa.pub id_ed.pub
 echo "test1"
-echo $PRIVATE_MINELABS_KEY | ssh -i /dev/stdin -o "StrictHostKeyChecking no" -P 2233 minelabs@minelabs.be<< EOF
-cd config
-put restart
-get restart.log
-bye
-EOF
+echo $PRIVATE_MINELABS_KEY | ssh -i /dev/stdin -o "StrictHostKeyChecking no" -P 2233 minelabs@minelabs.be <<< \
+ $"cd config \n put restart \n get restart.log \n bye"
 cat restart.log
