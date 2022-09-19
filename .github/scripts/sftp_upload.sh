e6cfdc4862be7738e3dbcc506be7e9d599d9f37a
Bert Jorissen <info@bertjorissen.be>
2022-09-19 19:29:56 +0200

    chmod

--- .github/scripts/sftp_upload.sh
+++ .github/scripts/sftp_upload.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 sudo apt-get install -y ssh
-echo $PUBLIC_MINELABS_KEY_ED >> id_ed.pub
-echo $PUBLIC_MINELABS_KEY_RSA >> id_rsa.pub
+#!echo $PUBLIC_MINELABS_KEY_ED >> id_ed.pub
+#!echo $PUBLIC_MINELABS_KEY_RSA >> id_rsa.pub
 
 #! ssh-copy-id -i -f id_ed.pub minelabs@minelabs.be
 #! ssh-copy-id -i -f id_rsa.pub minelabs@minelabs.be
