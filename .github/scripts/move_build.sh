97e91ca6f322c7dfb987c5ba947b5ece08cef52e
Bert Jorissen <info@bertjorissen.be>
2022-09-19 11:14:26 +0200

    test duso

--- .github/scripts/move_build.sh
+++ .github/scripts/move_build.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-mod_file=$(ls | grep -E "^(minelabs-)([0-9]+)(.)([0-9]+)(.)([0-9]+)(.jar)")
-source <(grep mod_version gradle.properties)
-source <(grep archives_base_name gradle.properties)
-source <(grep loader_version gradle.properties)
-source <(grep minecraft_version gradle.properties)
-echo ${archives_base_name}-${mod_version}.jar "---" $(mod_file)
-mkdir output
-cd output
-cp ../build/libs/${mod_file) .
-cd .. \
-echo "Moved mod to output"
+sudo mod_file=$(ls | grep -E "^(minelabs-)([0-9]+)(.)([0-9]+)(.)([0-9]+)(.jar)")
+sudo source <(grep mod_version gradle.properties)
+sudo source <(grep archives_base_name gradle.properties)
+sudo source <(grep loader_version gradle.properties)
+sudo source <(grep minecraft_version gradle.properties)
+sudo echo ${archives_base_name}-${mod_version}.jar "---" $(mod_file)
+sudo mkdir output
+sudo cd output
+sudo cp ../build/libs/${mod_file) .
+sudo cd .. \
+sudo echo "Moved mod to output"
