96e13117752862467af1b1e49d95632dba61ee5a
Bert Jorissen <info@bertjorissen.be>
2022-09-19 11:43:12 +0200

    test names less sudo

--- .github/scripts/sftp_upload.sh
+++ .github/scripts/sftp_upload.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
 sudo apt-get install -y ssh
-sudo ssh-copy-id -i ${{ secrets.PUBLIC_MINELABS_KEY_ED }} minelabs@minelabs.be
-sudo ssh-copy-id -i ${{ secrets.PUBLIC_MINELABS_KEY_RSA }} minelabs@minelabs.be
+ssh-copy-id -i ${{ secrets.PUBLIC_MINELABS_KEY_ED }} minelabs@minelabs.be
+ssh-copy-id -i ${{ secrets.PUBLIC_MINELABS_KEY_RSA }} minelabs@minelabs.be
 
-sudo echo "stop stop" >> restart
-sudo sftp -P 2233 -i ${{ secrets.PRIVATE_MINELABS_KEY}} << EOF
+echo "stop stop" >> restart
+sftp -P 2233 -i ${{ secrets.PRIVATE_MINELABS_KEY}} << EOF
 cd config
 put restart
 bye
 EOF
 
-sudo mod_file=$(ls | grep -E "^(minelabs-)([0-9]+)(.)([0-9]+)(.)([0-9]+)(.jar)")
-sudo sleep 3
-sudo echo "start start" >> restart
+mod_file=$(ls | grep -E "^(minelabs-)([0-9]+)(.)([0-9]+)(.)([0-9]+)(.jar)")
+sleep 3
+echo "start start" >> restart
 
-sudo sftp -P 2233 -i ${{ secrets.PRIVATE_MINELABS_KEY}} << EOF
+sftp -P 2233 -i ${{ secrets.PRIVATE_MINELABS_KEY}} << EOF
 cd minecraft-data
 rm minelabs*
 put $mod_file
@@ -23,4 +23,4 @@ put restart
 bye
 EOF
 
-sudo echo "Upload complete. "
+echo "Upload complete. "
