d19c810333bbb9bb059a59a3a316a323b0c01d38
Bert Jorissen <info@bertjorissen.be>
2022-09-19 18:50:44 +0200

    keys

--- .github/scripts/sftp_upload.sh
+++ .github/scripts/sftp_upload.sh
@@ -3,12 +3,13 @@ sudo apt-get install -y ssh
 echo $PUBLIC_MINELABS_KEY_ED >> id_ed.pub
 echo $PUBLIC_MINELABS_KEY_RSA >> id_rsa.pub
 
-ssh-copy-id -i -f id_ed.pub minelabs@minelabs.be
-ssh-copy-id -i -f id_rsa.pub minelabs@minelabs.be
+#! ssh-copy-id -i -f id_ed.pub minelabs@minelabs.be
+#! ssh-copy-id -i -f id_rsa.pub minelabs@minelabs.be
 
 echo "stop stop" >> restart
 
 echo $PRIVATE_MINELABS_KEY >> id_rsa
+echo $PRIVATE_MINELABS_KEY_pub >> id_rsa.pub
 sftp -P 2233 -i id_rsa minelabs@minelabs.be<< EOF
 cd config
 put restart
