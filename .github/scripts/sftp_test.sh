fd91972a5a1e206ae454523a9c24794117909248
Bert Jorissen <info@bertjorissen.be>
2022-09-19 19:51:38 +0200

    id_rsa

--- .github/scripts/sftp_test.sh
+++ .github/scripts/sftp_test.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-echo "stop stop" >> restart
+echo "stop stop" > restart
 rm -f id_rsa id_ed id_rsa.pub id_ed.pub
-echo $PRIVATE_MINELABS_KEY >> id_rsa
-chmod 400 id_rsa
-echo $PRIVATE_MINELABS_KEY_PUB >> "id_rsa.pub"
+echo $PRIVATE_MINELABS_KEY > id_rsa
+chmod 600 id_rsa
+#!echo $PRIVATE_MINELABS_KEY_PUB > "id_rsa.pub"
 sftp -o "StrictHostKeyChecking no" -P 2233 -i ./id_rsa minelabs@minelabs.be<< EOF
 cd config
 put restart
