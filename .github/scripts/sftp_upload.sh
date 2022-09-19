02743898223b22b3ed508e5b0789caf8d6311e65
Bert Jorissen <info@bertjorissen.be>
2022-09-19 12:09:19 +0200

    userame sftp

--- .github/scripts/sftp_upload.sh
+++ .github/scripts/sftp_upload.sh
@@ -3,13 +3,13 @@ sudo apt-get install -y ssh
 echo $PUBLIC_MINELABS_KEY_ED >> id_ed.pub
 echo $PUBLIC_MINELABS_KEY_RSA >> id_rsa.pub
 
-ssh-copy-id -i id_ed.pub minelabs@minelabs.be
-ssh-copy-id -i id_rsa.pub minelabs@minelabs.be
+ssh-copy-id -i -f id_ed.pub minelabs@minelabs.be
+ssh-copy-id -i -f id_rsa.pub minelabs@minelabs.be
 
 echo "stop stop" >> restart
 
 echo $PRIVATE_MINELABS_KEY >> id_rsa
-sftp -P 2233 -i id_rsa << EOF
+sftp -P 2233 -i id_rsa minelabs@minelabs.be<< EOF
 cd config
 put restart
 bye
@@ -19,7 +19,7 @@ mod_file=$(ls output| grep -E "^(minelabs-)([0-9]+)(.)([0-9]+)(.)([0-9]+)(.jar)"
 sleep 3
 echo "start start" >> restart
 
-sftp -P 2233 -i $PRIVATE_MINELABS_KEY << EOF
+sftp -P 2233 -i id_rsa  minelabs@minelabs.be<< EOF
 cd minecraft-data
 rm minelabs*
 put output/$mod_file
