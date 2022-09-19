b062b60a7a2fb0f0ab661a938f6917e5023037f2
Bert Jorissen <info@bertjorissen.be>
2022-09-19 21:48:03 +0200

    version env test

--- .github/scripts/sftp_upload.sh
+++ .github/scripts/sftp_upload.sh
@@ -11,25 +11,31 @@ cd ../minecraft-data/mods
 get *
 bye
 EOF
-
 rm restart
 rm $(ls | grep -E "^(minelabs-)([0-9]+)(.)([0-9]+)(.)([0-9]+)(.jar)")
+cd ..
+
 
 mod_file=$(ls output| grep -E "^(minelabs-)([0-9]+)(.)([0-9]+)(.)([0-9]+)(.jar)")
+echo $mod_file
 . <(grep archives_base_name gradle.properties)
 . <(grep loader_version gradle.properties)
 . <(grep minecraft_version gradle.properties)
 echo "MC_TYPE=FABRIC
 MC_VERSION=$minecraft_version
-MC_FABRIC_LOADER_VERSION=$loader_version" > ../minecraft.env
+MC_FABRIC_LOADER_VERSION=$loader_version" > minecraft.env
+cat minecraft.env
 sleep 3
-echo "start start" > ../restart
+echo "start start" > restart
+
+cd mods
+cp "../output/$mod_file" .
 
 sftp -o "StrictHostKeyChecking no" -P 2233  minelabs@minelabs.be<< EOF
 cd minecraft-data/mods
 rm *
 put *
-cd ../config
+cd ../../config
 put ../restart
 put ../minecraft.env
 bye
