ef5f8fe9bb93c5332e07bc21fa807469c26232f8
Bert Jorissen <info@bertjorissen.be>
2022-09-19 20:39:50 +0200

    env

--- .github/scripts/sftp_test.sh
+++ .github/scripts/sftp_test.sh
@@ -2,11 +2,7 @@
 echo "stop stop" > restart
 rm -f id_rsa id_ed id_rsa.pub id_ed.pub
 
-export SSH_AUTH_SOCK=~/.ssh/ssh-agent.sock
-ssh-add -l 2>/dev/null >/dev/null
-[ $? -ge 2 ] && ssh-agent -a "$SSH_AUTH_SOCK" >/dev/null
-ssh-add - <<< $$PRIVATE_MINELABS_KEY
-sftp -o "StrictHostKeyChecking no" -P 2233 minelabs@minelabs.be<< EOF
+echo $PRIVATE_MINELABS_KEY | ssh -i /dev/stdin -o "StrictHostKeyChecking no" -P 2233 minelabs@minelabs.be<< EOF
 cd config
 put restart
 get restart.log
