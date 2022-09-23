865900f1fbfb3a04e571f18bcfa6e15c3a96c1c8
pixar02 <pixar.pf@gmail.com>
2022-09-23 20:21:33 +0200

    working renderer

--- src/main/java/be/uantwerpen/minelabs/renderer/model/LabChestModel.java
+++ src/main/java/be/uantwerpen/minelabs/renderer/model/LabChestModel.java
@@ -9,15 +9,6 @@ import software.bernie.geckolib3.model.AnimatedGeoModel;
 public class LabChestModel extends AnimatedGeoModel<LabChestBlockEntity> {
     @Override
     public Identifier getModelResource(LabChestBlockEntity object) {
-        if (object.getCachedState().getBlock() == Blocks.LAB_DRAWER) { // DRAWER ANIMATION
-            return new Identifier(Minelabs.MOD_ID, "animations/lab_drawer.animation.json");
-        } else { // CABIN ANIMATION
-            return new Identifier(Minelabs.MOD_ID, "animations/lab_cabin.animation.json");
-        }
-    }
-
-    @Override
-    public Identifier getTextureResource(LabChestBlockEntity object) {
         if (object.getCachedState().getBlock() == Blocks.LAB_DRAWER) { // DRAWER MODEL
             return new Identifier(Minelabs.MOD_ID, "geo/lab_drawer.geo.json");
         } else { // CABIN ANIMATION
@@ -26,14 +17,16 @@ public class LabChestModel extends AnimatedGeoModel<LabChestBlockEntity> {
     }
 
     @Override
-    public Identifier getAnimationResource(LabChestBlockEntity animatable) {
-        // TODO MERGE TEXTURES
+    public Identifier getTextureResource(LabChestBlockEntity object) {
         return new Identifier(Minelabs.MOD_ID, "textures/block/lab_chest.png");
-        /*
-        if (animatable.getCachedState().getBlock() == Blocks.LAB_DRAWER){ // DRAWER MODEL
-            return new Identifier(Minelabs.MOD_ID, "textures/block/lab_drawer.png");
+    }
+
+    @Override
+    public Identifier getAnimationResource(LabChestBlockEntity animatable) {
+        if (animatable.getCachedState().getBlock() == Blocks.LAB_DRAWER) { // DRAWER ANIMATION
+            return new Identifier(Minelabs.MOD_ID, "animations/lab_drawer.animation.json");
         } else { // CABIN ANIMATION
-            return new Identifier(Minelabs.MOD_ID, "textures/block/lab_cabin.png");
-        }*/
+            return new Identifier(Minelabs.MOD_ID, "animations/lab_cabin.animation.json");
+        }
     }
 }
