d5f0787e8382709f6e89ad0de3bacb5f182de661
Joey De Pauw <joeydepauw@gmail.com>
2022-09-27 20:01:36 +0200

    cosmetic drawer keep state of open for animations

--- src/main/java/be/uantwerpen/minelabs/block/entity/LabChestBlockEntity.java
+++ src/main/java/be/uantwerpen/minelabs/block/entity/LabChestBlockEntity.java
@@ -1,18 +1,22 @@
 package be.uantwerpen.minelabs.block.entity;
 
+import be.uantwerpen.minelabs.Minelabs;
 import be.uantwerpen.minelabs.gui.lab_chest_gui.LabChestScreenHandler;
 import be.uantwerpen.minelabs.inventory.ImplementedInventory;
+import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.entity.BlockEntity;
+import net.minecraft.block.entity.ChestBlockEntity;
+import net.minecraft.block.entity.ViewerCountManager;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.entity.player.PlayerInventory;
+import net.minecraft.inventory.DoubleInventory;
 import net.minecraft.inventory.Inventories;
+import net.minecraft.inventory.Inventory;
 import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NbtCompound;
-import net.minecraft.screen.ArrayPropertyDelegate;
-import net.minecraft.screen.NamedScreenHandlerFactory;
-import net.minecraft.screen.PropertyDelegate;
-import net.minecraft.screen.ScreenHandler;
+import net.minecraft.screen.*;
+import net.minecraft.sound.SoundEvents;
 import net.minecraft.text.Text;
 import net.minecraft.util.collection.DefaultedList;
 import net.minecraft.util.math.BlockPos;
@@ -30,6 +34,45 @@ public class LabChestBlockEntity extends BlockEntity implements ImplementedInven
     private final DefaultedList<ItemStack> inventory = DefaultedList.ofSize(21, ItemStack.EMPTY);
     protected final PropertyDelegate propertyDelegate = new ArrayPropertyDelegate(0);
     private final AnimationFactory factory = new AnimationFactory(this);
+
+    private boolean open = false;
+
+    private final ViewerCountManager stateManager = new ViewerCountManager(){
+
+        @Override
+        protected void onContainerOpen(World world, BlockPos pos, BlockState state) {
+//            ChestBlockEntity.playSound(world, pos, state, SoundEvents.BLOCK_CHEST_OPEN);
+        }
+
+        @Override
+        protected void onContainerClose(World world, BlockPos pos, BlockState state) {
+//            ChestBlockEntity.playSound(world, pos, state, SoundEvents.BLOCK_CHEST_CLOSE);
+        }
+
+        @Override
+        protected void onViewerCountUpdate(World world, BlockPos pos, BlockState state, int oldViewerCount, int newViewerCount) {
+            LabChestBlockEntity.this.onInvOpenOrClose(world, pos, state, oldViewerCount, newViewerCount);
+        }
+
+        @Override
+        protected boolean isPlayerViewing(PlayerEntity player) {
+            if (player.currentScreenHandler instanceof GenericContainerScreenHandler) {
+                Inventory inventory = ((GenericContainerScreenHandler)player.currentScreenHandler).getInventory();
+                return inventory == LabChestBlockEntity.this || inventory instanceof DoubleInventory && ((DoubleInventory)inventory).isPart(LabChestBlockEntity.this);
+            }
+            return false;
+        }
+    };
+
+    @Override
+    public boolean onSyncedBlockEvent(int type, int data) {
+        Minelabs.LOGGER.info("viewcount: " + Integer.toString(data));
+        if (type == 1) {
+            this.open = (data > 0);
+            return true;
+        }
+        return super.onSyncedBlockEvent(type, data);
+    }
 
     public LabChestBlockEntity(BlockPos pos, BlockState state) {
         super(BlockEntities.LAB_CHEST_BLOCK_ENTITY, pos, state);
@@ -67,15 +110,23 @@ public class LabChestBlockEntity extends BlockEntity implements ImplementedInven
 
     }
 
+    protected void onInvOpenOrClose(World world, BlockPos pos, BlockState state, int oldViewerCount, int newViewerCount) {
+        Block block = state.getBlock();
+        world.addSyncedBlockEvent(pos, block, 1, newViewerCount);
+    }
+
     @Override
     public void onOpen(PlayerEntity player) {
-        //TODO TRIGGER ANIMATION: OPEN
+        if (!this.removed && !player.isSpectator()) {
+            this.stateManager.openContainer(player, this.getWorld(), this.getPos(), this.getCachedState());
+        }
     }
 
     @Override
     public void onClose(PlayerEntity player) {
-        //TODO TRIGGER ANIMATION: OPEN
-
+        if (!this.removed && !player.isSpectator()) {
+            this.stateManager.closeContainer(player, this.getWorld(), this.getPos(), this.getCachedState());
+        }
     }
 
     @Override
@@ -84,18 +135,27 @@ public class LabChestBlockEntity extends BlockEntity implements ImplementedInven
                 (this, "controllerOpen", 20, this::predicateOpen));
         animationData.addAnimationController(new AnimationController<LabChestBlockEntity>
                 (this, "controllerClose", 20, this::predicateClose));
-
     }
 
     //TODO TRIGGERS/EVENTS
     private <E extends IAnimatable> PlayState predicateOpen(AnimationEvent<E> event) {
-        event.getController().setAnimation(new AnimationBuilder().addAnimation("open"));
-        return PlayState.STOP;
+        if (this.open){
+            event.getController().setAnimation(new AnimationBuilder().addAnimation("open"));
+            return PlayState.CONTINUE;
+        }else{
+            event.getController().markNeedsReload();
+            return PlayState.STOP;
+        }
     }
 
     private <E extends IAnimatable> PlayState predicateClose(AnimationEvent<E> event) {
-        event.getController().setAnimation(new AnimationBuilder().addAnimation("close"));
-        return PlayState.STOP;
+        if (!this.open){
+            event.getController().setAnimation(new AnimationBuilder().addAnimation("close"));
+            return PlayState.CONTINUE;
+        }else{
+            event.getController().markNeedsReload();
+            return PlayState.STOP;
+        }
     }
 
     @Override
