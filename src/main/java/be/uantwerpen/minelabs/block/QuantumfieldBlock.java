f2281350cb58deb94ff8a68581adeafff119ffb3
pixar02 <pixar.pf@gmail.com>
2022-10-23 23:08:17 +0200

    fix issue #346 + some cleanup

--- src/main/java/be/uantwerpen/minelabs/block/QuantumfieldBlock.java
+++ src/main/java/be/uantwerpen/minelabs/block/QuantumfieldBlock.java
@@ -1,6 +1,7 @@
 package be.uantwerpen.minelabs.block;
 
 import be.uantwerpen.minelabs.block.entity.QuantumFieldBlockEntity;
+import be.uantwerpen.minelabs.util.MinelabsProperties;
 import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
 import net.minecraft.block.*;
 import net.minecraft.block.entity.BlockEntity;
@@ -9,24 +10,28 @@ import net.minecraft.item.ItemStack;
 import net.minecraft.server.world.ServerWorld;
 import net.minecraft.state.StateManager;
 import net.minecraft.state.property.BooleanProperty;
+import net.minecraft.state.property.IntProperty;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Direction;
 import net.minecraft.util.math.random.Random;
-import net.minecraft.world.BlockView;
 import net.minecraft.world.World;
 import net.minecraft.world.WorldAccess;
 import org.jetbrains.annotations.Nullable;
 
-import static be.uantwerpen.minelabs.block.entity.QuantumFieldBlockEntity.*;
-
-
 public class QuantumfieldBlock extends AbstractGlassBlock implements BlockEntityProvider {
-    public static final BooleanProperty MASTER = BooleanProperty.of("is_master");
+    public static final int MAX_AGE = 10;
+    public static final int decayrate = 1;
+    //    max 15 in minecraft
+    public static final int max_light = 15;
+    public static final int min_light = 0;
+    public static final BooleanProperty MASTER = MinelabsProperties.MASTER;
+    public static final IntProperty AGE = IntProperty.of("age", 0, 10);
+
 
     public QuantumfieldBlock() {
         // Properties of all quantumfield blocks
         // Change the first value in strength to get the wanted mining speed
-        super(FabricBlockSettings.of(Material.METAL).strength(0.5f, 2.0f).ticksRandomly().luminance(state -> Math.round(min_light + ((float)(max_age - state.get(AGE) - 1 ) / max_age) * (max_light-min_light))));
+        super(FabricBlockSettings.of(Material.METAL).strength(0.5f, 2.0f).ticksRandomly().luminance(state -> Math.round(min_light + ((float) (MAX_AGE - state.get(AGE) - 1) / MAX_AGE) * (max_light - min_light))));
         this.setDefaultState(getDefaultState().with(AGE, 0).with(MASTER, false));
     }
 
@@ -35,18 +40,13 @@ public class QuantumfieldBlock extends AbstractGlassBlock implements BlockEntity
     }
 
     @Override
-    public boolean isTranslucent(BlockState state, BlockView world, BlockPos pos) {
-        return super.isTranslucent(state, world, pos);
-    }
-
-    @Override
     public void randomTick(BlockState state, ServerWorld world, BlockPos pos, Random random) {
         if (!isMaster(state)) return;
 
         int age = state.get(AGE);
-        age = Math.min(age+decayrate, max_age);
-        if (age == max_age){
-            world.createAndScheduleBlockTick(pos,this,5);
+        age = Math.min(age + decayrate, MAX_AGE);
+        if (age == MAX_AGE) {
+            world.createAndScheduleBlockTick(pos, this, 5);
         }
         state = state.with(AGE, age);
         world.setBlockState(pos, state, Block.NOTIFY_ALL);
@@ -57,8 +57,8 @@ public class QuantumfieldBlock extends AbstractGlassBlock implements BlockEntity
         int age = state.get(AGE);
         if (state.getBlock() == neighborState.getBlock() && age < neighborState.get(AGE)) {
             age = neighborState.get(AGE);
-            if (age == max_age){
-                world.createAndScheduleBlockTick(pos,this,5);
+            if (age == MAX_AGE) {
+                world.createAndScheduleBlockTick(pos, this, 5);
             }
             return state.with(AGE, age);
         }
@@ -76,10 +76,10 @@ public class QuantumfieldBlock extends AbstractGlassBlock implements BlockEntity
     }
 
     public void removeQuantumBlockIfNeeded(BlockState state, ServerWorld world, BlockPos pos){
-        if (max_age == state.get(AGE)){
-            world.removeBlock(pos,false);
+        if (MAX_AGE == state.get(AGE)) {
+            world.removeBlock(pos, false);
             if (pos.getY() == AtomicFloor.AtomicFloorLayer) {
-                world.setBlockState(pos, Blocks.ATOM_FLOOR.getDefaultState(),Block.NO_REDRAW);
+                world.setBlockState(pos, Blocks.ATOM_FLOOR.getDefaultState(), Block.NO_REDRAW);
             }
         }
     }
--- src/main/java/be/uantwerpen/minelabs/util/MinelabsProperties.java
+++ src/main/java/be/uantwerpen/minelabs/util/MinelabsProperties.java
@@ -8,7 +8,7 @@ import net.minecraft.state.property.IntProperty;
 
 public class MinelabsProperties {
     public static final EnumProperty<BohrPart> BOHR_PART = EnumProperty.of("part", BohrPart.class);
-
+    public static final BooleanProperty MASTER = BooleanProperty.of("master");
     public static final IntProperty FIRE_COLOR = IntProperty.of("fire_color", 0, 10);
 
     public static final BooleanProperty ZOOMED = BooleanProperty.of("zoom");
