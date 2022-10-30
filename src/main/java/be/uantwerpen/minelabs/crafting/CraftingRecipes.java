e031e7a8435c526b72d1a8e125ef6e0a507d1611
pixar02 <pixar.pf@gmail.com>
2022-10-30 11:35:28 +0100

    fixes types not properly registered

--- src/main/java/be/uantwerpen/minelabs/crafting/CraftingRecipes.java
+++ src/main/java/be/uantwerpen/minelabs/crafting/CraftingRecipes.java
@@ -3,19 +3,27 @@ package be.uantwerpen.minelabs.crafting;
 import be.uantwerpen.minelabs.Minelabs;
 import be.uantwerpen.minelabs.crafting.ionic.IonicRecipe;
 import be.uantwerpen.minelabs.crafting.lewis.MoleculeRecipe;
-import net.minecraft.recipe.RecipeType;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
 
 public class CraftingRecipes {
 
-    public static RecipeType<IonicRecipe> IONIC_CRAFTING = RecipeType.register("ionic_crating");
-    public static RecipeType<MoleculeRecipe> MOLECULE_CRAFTING = RecipeType.register("molecule_crafting");
-
     public static void register() {
-        Registry.register(Registry.RECIPE_SERIALIZER, new Identifier(Minelabs.MOD_ID, "ionic_crafting"), IonicRecipe.IonicRecipeSerializer.INSTANCE);
-        Registry.register(Registry.RECIPE_SERIALIZER, new Identifier(Minelabs.MOD_ID, "molecule_crafting"), MoleculeRecipe.MoleculeRecipeSerializer.INSTANCE);
+        // Ionic Recipe
+        Registry.register(Registry.RECIPE_SERIALIZER,
+                new Identifier(Minelabs.MOD_ID, IonicRecipe.IonicRecipeSerializer.ID),
+                IonicRecipe.IonicRecipeSerializer.INSTANCE);
+        Registry.register(Registry.RECIPE_TYPE,
+                new Identifier(Minelabs.MOD_ID, IonicRecipe.IonicRecipeType.ID),
+                IonicRecipe.IonicRecipeType.INSTANCE);
+
+        //Lewis Recipe
+        Registry.register(Registry.RECIPE_SERIALIZER,
+                new Identifier(Minelabs.MOD_ID, MoleculeRecipe.MoleculeRecipeSerializer.ID),
+                MoleculeRecipe.MoleculeRecipeSerializer.INSTANCE);
+        Registry.register(Registry.RECIPE_TYPE,
+                new Identifier(Minelabs.MOD_ID, MoleculeRecipe.MoleculeRecipeType.ID),
+                MoleculeRecipe.MoleculeRecipeType.INSTANCE);
+
     }
-
-
 }
