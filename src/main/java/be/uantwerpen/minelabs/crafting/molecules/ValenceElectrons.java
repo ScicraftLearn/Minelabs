5da7fa9af9cfff23e24d19fd1585bd9d699847bc
pixar02 <pixar.pf@gmail.com>
2022-11-22 11:08:53 +0100

    final fix valence render

--- src/main/java/be/uantwerpen/minelabs/crafting/molecules/ValenceElectrons.java
+++ src/main/java/be/uantwerpen/minelabs/crafting/molecules/ValenceElectrons.java
@@ -18,7 +18,7 @@ public class ValenceElectrons {
     /**
      * @param bondDirections : amount of bonds in which direction
      * @param valenceE       : amount of electrons on the current atom
-     * @param forced         : True = Use 1's, False = use pairs
+     * @param forced         : True = use pairs, False = Use 1's
      */
     public ValenceElectrons(Map<String, Integer> bondDirections, int valenceE, boolean forced) {
         this.totalElectronCount = valenceE;
@@ -32,9 +32,9 @@ public class ValenceElectrons {
     }
 
     /**
-     * @param bondDirections
-     * @param count
-     * @param forced         : True = Use 1's, False = use pairs
+     * @param bondDirections : amount of bonds in which direction
+     * @param count          : amount of electrons to place around the atom
+     * @param forced         : True = use pairs, False = Use 1's
      */
     private void addElectrons(Map<String, Integer> bondDirections, int count, boolean forced) {
         //inverse bonddir & voorkeur N-E-S-W
@@ -57,13 +57,28 @@ public class ValenceElectrons {
         }
     }
 
+    /**
+     * @param bondDirections : amount of bonds in which direction
+     * @param forced         : True = use pairs, False = Use 1's
+     * @return A list contain a set order to iterate over
+     */
     private List<String> getBestList(Map<String, Integer> bondDirections, boolean forced) {
-        if (bondDirections.get("n") != 0 || bondDirections.get("s") != 0) {
-            return Arrays.asList("e", "w", "n", "s");
-        } else if (bondDirections.get("e") != 0 || bondDirections.get("w") != 0) {
-            return Arrays.asList("n", "s", "e", "w");
+        if (totalElectronCount > 3 || (totalElectronCount == 2 && !forced)) {
+            if (bondDirections.get("n") != 0 || bondDirections.get("s") != 0) {
+                return Arrays.asList("e", "w", "n", "s");
+            } else if (bondDirections.get("e") != 0 || bondDirections.get("w") != 0) {
+                return Arrays.asList("n", "s", "e", "w");
+            } else {
+                return Arrays.asList("n", "e", "s", "w");
+            }
         } else {
-            return Arrays.asList("n", "e", "s", "w");
+            if (bondDirections.get("n") != 0 || bondDirections.get("s") != 0) {
+                return Arrays.asList("n", "s", "e", "w");
+            } else if (bondDirections.get("e") != 0 || bondDirections.get("w") != 0) {
+                return Arrays.asList("e", "w", "n", "s");
+            } else {
+                return Arrays.asList("n", "e", "s", "w");
+            }
         }
     }
 
