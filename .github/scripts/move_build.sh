0e8979089747f151ba9d84551e9b84d82c148e61
Bert Jorissen <bert.jorissen@uantwerpen.be>
2022-11-25 14:43:39 +0100

    test github action

--- .github/scripts/move_build.sh
+++ .github/scripts/move_build.sh
@@ -7,6 +7,6 @@ mod_file=$(ls build/libs| grep -E "^(minelabs-)([0-9]+)(.)([0-9]+)(.)([0-9]+)(.j
 echo "$archives_base_name-$mod_version.jar --- $mod_file"
 mkdir output
 cd output
-cp "../build/libss/$mod_file" . || (echo "::error::Can't move the mod"; return -1)
+cp "../build/libss/$mod_file" . || (echo "::error::Can't move the mod"; exit -1)
 cd ..
 echo "Moved mod to output.."
