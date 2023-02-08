f5fef5ff84fa1346971a2a1f98fc6ab5fbac680b
Robbe Nooyens <robnoo@robbes-mbp.uantwerpen.be>
2023-02-08 01:29:05 +0100

    Custom particles and effect for gasses

---
+++ src/main/java/be/uantwerpen/minelabs/potion/GasEntity.java
@@ -0,0 +1,74 @@
+package be.uantwerpen.minelabs.potion;
+
+import be.uantwerpen.minelabs.Minelabs;
+import be.uantwerpen.minelabs.item.Items;
+import com.mojang.serialization.Codec;
+import net.minecraft.block.Block;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.Blocks;
+import net.minecraft.block.Material;
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.particle.ParticleManager;
+import net.minecraft.entity.EntityType;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.projectile.thrown.PotionEntity;
+import net.minecraft.entity.projectile.thrown.ThrownItemEntity;
+import net.minecraft.fluid.Fluids;
+import net.minecraft.item.Item;
+import net.minecraft.particle.ParticleType;
+import net.minecraft.particle.ParticleTypes;
+import net.minecraft.util.Identifier;
+import net.minecraft.util.hit.BlockHitResult;
+import net.minecraft.util.hit.HitResult;
+import net.minecraft.world.World;
+import net.minecraft.world.explosion.Explosion;
+
+
+public class GasEntity extends PotionEntity {
+
+    private final String molecule;
+
+    public GasEntity(World world, LivingEntity player, String molecule) {
+//        super(EntityType.POTION, player, world);
+        super(world, player);
+        this.molecule = molecule;
+    }
+
+    @Override
+    protected Item getDefaultItem() {
+        return Items.ERLENMEYER;
+    }
+
+    @Override
+    protected void onBlockHit(BlockHitResult blockHitResult) {
+        // TODO: action based on molecule
+        BlockState blockState = this.world.getBlockState(blockHitResult.getBlockPos());
+        if (blockState.getMaterial() == Material.STONE) {
+            this.world.createExplosion(null, this.getX(), this.getY(), this.getZ(), 2, false, Explosion.DestructionType.DESTROY);
+        }
+        if (blockState.getMaterial() == Material.METAL) {
+            this.world.createExplosion(null, this.getX(), this.getY(), this.getZ(), 4, true, Explosion.DestructionType.DESTROY);
+        }
+    }
+
+    @Override
+    protected void onCollision(HitResult hitResult) {
+        // This is from ProjectileEntity.onCollision (super.super.onCollision)
+        if (hitResult instanceof BlockHitResult) {
+            this.onBlockHit((BlockHitResult) hitResult);
+        }
+        if (!this.world.isClient) {
+            for (int i = 0; i < 20; i++) {
+                double offsetX = (Math.random() - 0.5) * 0.5;
+                double offsetY = (Math.random() - 0.5) * 0.5;
+                double offsetZ = (Math.random() - 0.5) * 0.5;
+                // TODO: Add custom particles https://fabricmc.net/wiki/tutorial:particles
+                // TODO: Add colors based on molecule
+                MinecraftClient.getInstance().particleManager.addParticle(ParticleTypes.SPLASH,
+                        this.getX() + offsetX, this.getY() + offsetY, this.getZ() + offsetZ,
+                        0, 0, 0);
+            }
+            this.discard();
+        }
+    }
+}
--- src/main/java/be/uantwerpen/minelabs/potion/GasPotion.java
+++ src/main/java/be/uantwerpen/minelabs/potion/GasPotion.java
@@ -1,6 +1,5 @@
 package be.uantwerpen.minelabs.potion;
 
-import be.uantwerpen.minelabs.Minelabs;
 import be.uantwerpen.minelabs.advancement.criterion.Criteria;
 import be.uantwerpen.minelabs.advancement.criterion.ErlenmeyerCriterion;
 import be.uantwerpen.minelabs.item.IMoleculeItem;
@@ -10,6 +9,7 @@ import net.minecraft.item.*;
 import net.minecraft.potion.Potion;
 import net.minecraft.potion.PotionUtil;
 import net.minecraft.server.network.ServerPlayerEntity;
+import net.minecraft.stat.Stats;
 import net.minecraft.util.ActionResult;
 import net.minecraft.util.Hand;
 import net.minecraft.util.TypedActionResult;
@@ -40,12 +40,31 @@ public class GasPotion extends LingeringPotionItem implements IMoleculeItem {
 
     @Override
     public TypedActionResult<ItemStack> use(World world, PlayerEntity user, Hand hand) {
-        TypedActionResult<ItemStack> result = super.use(world, user, hand);
+        ItemStack itemStack = user.getStackInHand(hand);
+        if (!world.isClient) {
+            GasEntity gasEntity = new GasEntity(world, user, this.molecule);
+            gasEntity.setItem(itemStack);
+            gasEntity.setVelocity(user, user.getPitch(), user.getYaw(), -20.0F, 0.5F, 1.0F);
+            world.spawnEntity(gasEntity);
+        }
+
+        user.incrementStat(Stats.USED.getOrCreateStat(this));
+        if (!user.getAbilities().creativeMode) {
+            itemStack.decrement(1);
+        }
+
         // Advancement
-        if (result.getResult().isAccepted() && user instanceof ServerPlayerEntity serverPlayer){
+        if(user instanceof ServerPlayerEntity serverPlayer) {
             Criteria.ERLENMEYER_CRITERION.trigger(serverPlayer, ErlenmeyerCriterion.Type.THROW);
         }
-        return result;
+
+        return TypedActionResult.success(itemStack, world.isClient());
+//        TypedActionResult<ItemStack> result = super.use(world, user, hand);
+//        // Advancement
+//        if (result.getResult().isAccepted() && user instanceof ServerPlayerEntity serverPlayer){
+//            Criteria.ERLENMEYER_CRITERION.trigger(serverPlayer, ErlenmeyerCriterion.Type.THROW);
+//        }
+//        return result;
     }
 
     @Override
