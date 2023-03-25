4890f3f8b48ca652352b4422039b19eace4b06b4
Bert Jorissen <info@bertjorissen.be>
2023-03-25 21:49:32 +0100

    Rescale arrows - zero field -> make arrow disappear - non-zero field -> scale between 0 and 3 (insted of 1) - use distance instead of sum of components - full update field after movement TODO - zero field: make arrow gray - large field: square with field (going continuous in first derivative) and turn red - reimplement routine, now rather slow

--- src/main/java/be/uantwerpen/minelabs/block/entity/ChargedBlockEntity.java
+++ src/main/java/be/uantwerpen/minelabs/block/entity/ChargedBlockEntity.java
@@ -143,10 +143,13 @@ public class ChargedBlockEntity extends BlockEntity{
             // therefore they must not be recalculated after the time freeze stops
             if(!afterTimeFreeze) {
                 if (world.getBlockEntity(pos_block) instanceof ElectricFieldSensorBlockEntity sensor && !pos.equals(pos_block) ){
-                    Vec3f vec_pos = new Vec3f(pos.getX()-pos_block.getX(), pos.getY()-pos_block.getY(), pos.getZ()-pos_block.getZ());
-                    float d_E = (float) ((getCharge() * 1 * kc) / Math.pow(vec_pos.dot(vec_pos), 1.5));
-                    vec_pos.scale(d_E);
-                    sensor.getField().subtract(vec_pos);
+                    sensor.calculateField(world, pos_block, pos);
+
+                    // Vec3f vec_pos = new Vec3f(pos.getX()-pos_block.getX(), pos.getY()-pos_block.getY(), pos.getZ()-pos_block.getZ());
+                    // float d_E = (float) ((getCharge() * 1 * kc) / Math.pow(vec_pos.dot(vec_pos), 1.5));
+                    // vec_pos.scale(d_E);
+                    // sensor.getField().subtract(vec_pos);
+
                     sensor.markDirty();
                     sensor.sync();
                 }
--- src/main/java/be/uantwerpen/minelabs/renderer/ElectricFieldSensorRenderer.java
+++ src/main/java/be/uantwerpen/minelabs/renderer/ElectricFieldSensorRenderer.java
@@ -8,47 +8,51 @@ import net.minecraft.client.render.model.json.ModelTransformation;
 import net.minecraft.client.util.math.MatrixStack;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.math.Direction;
+import net.minecraft.util.math.MathHelper;
 import net.minecraft.util.math.Vec3f;
 
 public class ElectricFieldSensorRenderer implements BlockEntityRenderer<ElectricFieldSensorBlockEntity> {
 
     private final BlockEntityRendererFactory.Context context;
+    private final static float max_field = 3;
 
     public ElectricFieldSensorRenderer(BlockEntityRendererFactory.Context ctx) {
         this.context = ctx;
     }
 
     @Override
-    public void render(ElectricFieldSensorBlockEntity entity, float tickDelta, MatrixStack matrices, VertexConsumerProvider vertexConsumers, int light, int overlay) {
+    public void render(ElectricFieldSensorBlockEntity entity, float tickDelta, MatrixStack matrices,
+                       VertexConsumerProvider vertexConsumers, int light, int overlay) {
         Vec3f field = entity.getField().copy();
-        ItemStack arrow = new ItemStack(net.minecraft.item.Items.ARROW);
 
         matrices.push();
 
-        // Center arrow in block
-        matrices.translate(0.5, 0.5, 0.5);
-
-        // We want the arrows to point the other direction, so we turn the field vector around.
-        field.scale(-1);
-
         if(field.equals(Vec3f.ZERO)) {
-            // default field should be north iso east.
-            // positive x is east, so we want to rotate -90 degrees along the y-axis.
-            matrices.multiply(Direction.UP.getUnitVector().getDegreesQuaternion(90));
-        }else{
+            // Don't display arrow if the field is zero
+            matrices.scale(0f, 0f, 0f);
+        } else {
+            // Center arrow in block
+            matrices.translate(0.5, 0.5, 0.5);
+
+            // We want the arrows to point the other direction, so we turn the field vector around.
+            field.scale(-1);
+
             /*
-             * This algorithm determines the normal vector of the plane described by the original orientation of the arrow (v) and the target direction (field).
-             * It then rotates around this vector with the angle theta between the two vectors to point the arrow in the direction of the field.
+             * This algorithm determines the normal vector of the plane described by the original orientation
+             * of the arrow (v) and the target direction (field).
+             * It then rotates around this vector with the angle theta between the two vectors to point
+             * the arrow in the direction of the field.
+             *
+             * By default, the arrow points in negative x (WEST)
              */
-            // By default the arrow points in negative x (WEST)
-            Vec3f v = new Vec3f(-1, 0, 0);
+            Vec3f v = Direction.WEST.getUnitVector();
 
             // Compute theta with cosine formula.
-            double theta = Math.acos(v.dot(field) / Math.sqrt(Math.pow(field.getX(), 2) + Math.pow(field.getY(), 2) + Math.pow(field.getZ(), 2)));
+            double theta = Math.acos(v.dot(field) * MathHelper.fastInverseSqrt(field.dot(field)));
 
             if (theta == 0 || theta == Math.PI) {
                 // When the two vectors are parallel, their cross product does not produce the normal vector of the plane.
-                // Instead we set in to one of the infinite valid normal vectors: positive Y.
+                // Instead, we set in to one of the infinite valid normal vectors: positive Y.
                 v = Direction.UP.getUnitVector();
             } else {
                 v.cross(field);
@@ -56,22 +60,28 @@ public class ElectricFieldSensorRenderer implements BlockEntityRenderer<Electric
             }
 
             matrices.multiply(v.getRadialQuaternion((float)theta));
+
+            // correct for default arrow inclination of 45 degrees.
+            matrices.multiply(Direction.SOUTH.getUnitVector().getDegreesQuaternion(-45));
+
+            // make sure arrows are larger when near charged blocks and smaller when further away
+            float reference = (float) Math.sqrt(field.dot(field)) / max_field;
+
+            if (reference > .8f) {
+                matrices.scale(1f,1f,1f);
+            } else {
+                matrices.scale(
+                        Math.min(reference + 0.2f, 1f),
+                        Math.min(reference + 0.2f, 1f),
+                        Math.min(reference + 0.2f, 1f)
+                );
+            }
         }
 
-        // correct for default arrow inclination of 45 degrees.
-        Vec3f x_rotation = new Vec3f(0, 0, 1);
-        matrices.multiply(x_rotation.getDegreesQuaternion(-45));
-
-        // make sure arrows are larger when near charged blocks and smaller when further away
-        float reference = Math.abs(field.getX()) + Math.abs(field.getY()) + Math.abs(field.getZ());
-        if (reference > 1) {
-            matrices.scale(1,1,1);
-            //matrices.scale(reference, reference, reference);
-        } else {
-            matrices.scale(Math.min(reference+0.2f, 0.99f),Math.min(reference+0.2f, 0.99f),Math.min(reference+0.2f, 0.99f));
-        }
-
-        context.getItemRenderer().renderItem(arrow, ModelTransformation.Mode.GUI, light, overlay, matrices, vertexConsumers, 0);
+        ItemStack arrow = new ItemStack(net.minecraft.item.Items.ARROW);
+        context.getItemRenderer().renderItem(
+                arrow, ModelTransformation.Mode.GUI, light, overlay, matrices, vertexConsumers, 0
+        );
 
         matrices.pop();
     }
