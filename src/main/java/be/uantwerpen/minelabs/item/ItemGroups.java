7533e90769f3030212d21e962f837800bda897cf
Bert Jorissen <info@bertjorissen.be>
2023-03-26 22:19:53 +0200

    Added Lasertool to Minelabs-ItemGroup. (to be cleaned up in switch to 19.3) #424

--- src/main/java/be/uantwerpen/minelabs/item/ItemGroups.java
+++ src/main/java/be/uantwerpen/minelabs/item/ItemGroups.java
@@ -7,9 +7,36 @@ import net.minecraft.item.ItemStack;
 import net.minecraft.util.Identifier;
 
 public class ItemGroups {
+    // Hard-coded ItemGroup, update from 19.3 allows to use ItemGroupEvents to extend ItemGroups. Currently, only
+    //  addition by had is allowed or automatic addition (in Items.Settings). You cant combine both because of the
+    //  implementation within the Fabric-API.
     public static final ItemGroup MINELABS = FabricItemGroupBuilder.create(
                     new Identifier(Minelabs.MOD_ID, "minelabs"))
             .icon(() -> new ItemStack(Items.MOLOGRAM))
+            .appendItems(stack -> {
+                stack.add(new ItemStack(Items.ATOM_FLOOR));
+                stack.add(new ItemStack(Items.SAFETY_GLASSES));
+                stack.add(new ItemStack(Items.LAB_COAT));
+                stack.add(new ItemStack(Items.LAB_COUNTER));
+                stack.add(new ItemStack(Items.LAB_DRAWER));
+                stack.add(new ItemStack(Items.LAB_SINK));
+                stack.add(new ItemStack(Items.LAB_CENTER));
+                stack.add(new ItemStack(Items.LAB_CORNER));
+                stack.add(new ItemStack(Items.MICROSCOPE));
+                stack.add(new ItemStack(Items.TUBERACK));
+                stack.add(new ItemStack(Items.LENS));
+                stack.add(new ItemStack(Items.BIG_LENS));
+                stack.add(new ItemStack(Items.TIME_FREEZE_BLOCK));
+                stack.add(new ItemStack(Items.ELECTRIC_FIELD_SENSOR));
+                stack.add(new ItemStack(Items.MOLOGRAM));
+                stack.add(new ItemStack(Items.BALLOON));
+                stack.add(new ItemStack(Items.BOHR_BLOCK));
+                stack.add(new ItemStack(Items.LEWIS_BLOCK_ITEM));
+                stack.add(new ItemStack(Items.IONIC_BLOCK_ITEM));
+                stack.add(new ItemStack(Items.LASERTOOL_IRON));
+                stack.add(new ItemStack(Items.LASERTOOL_GOLD));
+                stack.add(new ItemStack(Items.LASERTOOL_DIAMOND));
+            })
             .build();
 
     public static final ItemGroup CHEMICALS = FabricItemGroupBuilder.create(
