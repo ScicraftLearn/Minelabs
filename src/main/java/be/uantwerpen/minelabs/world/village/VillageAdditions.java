60713eeee953a60246c1e3e96255ef54b27ead3f
pixar02 <pixar.pf@gmail.con>
2023-04-11 12:01:39 +0200

    add todo

--- src/main/java/be/uantwerpen/minelabs/world/village/VillageAdditions.java
+++ src/main/java/be/uantwerpen/minelabs/world/village/VillageAdditions.java
@@ -7,6 +7,8 @@ import net.minecraft.util.Identifier;
 public class VillageAdditions {
 
     public static void registerVillageStructures(){
+        //TODO OTHER (BIOME) HOUSES
+        // add LEWIS BLOCK to structure
         FabricStructurePoolRegistry.registerSimple(
                 new Identifier("minecraft:village/plains/houses"),
                 new Identifier(Minelabs.MOD_ID, "plains_scientist_1"),
