82759218539bcdb964705f6c61824dddcba8afb3
pixar02 <pixar.pf@gmail.con>
2023-04-13 19:39:01 +0200

    added desert house for science villager

--- src/main/java/be/uantwerpen/minelabs/world/village/VillageAdditions.java
+++ src/main/java/be/uantwerpen/minelabs/world/village/VillageAdditions.java
@@ -7,7 +7,8 @@ import net.minecraft.util.Identifier;
 public class VillageAdditions {
 
     public static void registerVillageStructures(){
-        //TODO OTHER (BIOME: DESERT & TAIGA) HOUSES
+        //TODO OTHER (BIOME: TAIGA) HOUSES
+        // CHECK HOUSE/PROFESSION POOL
         FabricStructurePoolRegistry.registerSimple(
                 new Identifier("minecraft:village/plains/houses"),
                 new Identifier(Minelabs.MOD_ID, "village/plains_scientist_1"),
@@ -23,6 +24,10 @@ public class VillageAdditions {
         FabricStructurePoolRegistry.registerSimple(
                 new Identifier("minecraft:village/snowy/houses"),
                 new Identifier(Minelabs.MOD_ID, "village/snowy_scientist_2"),
+                2);
+        FabricStructurePoolRegistry.registerSimple(
+                new Identifier("minecraft:village/desert/houses"),
+                new Identifier(Minelabs.MOD_ID, "village/desert_scientist_1"),
                 2);
     }
 }
