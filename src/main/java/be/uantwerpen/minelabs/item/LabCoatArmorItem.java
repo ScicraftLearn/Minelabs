312d1294a8f6b86e6fdd52854bef49897bf9bafa
pixar02 <pixar.pf@gmail.con>
2023-05-08 21:07:58 +0200

    geckolib progression

--- src/main/java/be/uantwerpen/minelabs/item/LabCoatArmorItem.java
+++ src/main/java/be/uantwerpen/minelabs/item/LabCoatArmorItem.java
@@ -1,38 +1,39 @@
 package be.uantwerpen.minelabs.item;
 
-import net.minecraft.entity.EquipmentSlot;
-import net.minecraft.entity.LivingEntity;
-import net.minecraft.entity.decoration.ArmorStandEntity;
 import net.minecraft.item.ArmorItem;
 import net.minecraft.item.ArmorMaterial;
-import net.minecraft.item.Item;
 import software.bernie.geckolib.animatable.GeoItem;
 import software.bernie.geckolib.core.animatable.GeoAnimatable;
 import software.bernie.geckolib.core.animatable.instance.AnimatableInstanceCache;
-import software.bernie.geckolib.core.animation.AnimatableManager;
-import software.bernie.geckolib.core.animation.AnimationController;
+import software.bernie.geckolib.core.animatable.instance.SingletonAnimatableInstanceCache;
+import software.bernie.geckolib.core.animation.*;
 import software.bernie.geckolib.core.object.PlayState;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.function.Consumer;
+import java.util.function.Supplier;
 
 public class LabCoatArmorItem extends ArmorItem implements GeoAnimatable, GeoItem {
-    private final AnimationFactory factory = new AnimationFactory(this);
+
+    private final AnimatableInstanceCache cache = new SingletonAnimatableInstanceCache(this);
+
+    private final Supplier<Object> renderProvider = GeoItem.makeRenderer(this);
 
     public LabCoatArmorItem(ArmorMaterial material, Type slot, Settings settings) {
         super(material, slot, settings);
     }
 
     // Predicate runs every frame
-    private <P extends IAnimatable> PlayState predicate(AnimationEvent<P> event) {
+    /* TODO CHECK ?
+    private <AnimationStateHandler> PlayState predicate(AnimationState state) {
+
         // This is all the extradata this event carries. The livingentity is the entity
         // that's wearing the armor. The itemstack and equipmentslottype are self
         // explanatory.
-        LivingEntity livingEntity = event.getExtraDataOfType(LivingEntity.class).get(0);
+        LivingEntity livingEntity = state.getExtraDataOfType(LivingEntity.class).get(0);
 
         // Always loop the animation but later on in this method we'll decide whether or
         // not to actually play it
-        event.getController().setAnimation(new AnimationBuilder().addAnimation("idle", true));
+        state.getController().setAnimation(new AnimationBuilder().addAnimation("idle", true));
 
         // If the living entity is an armorstand just play the animation nonstop
         if (livingEntity instanceof ArmorStandEntity) {
@@ -53,12 +54,33 @@ public class LabCoatArmorItem extends ArmorItem implements GeoAnimatable, GeoIte
         // Make sure the player is wearing all the armor. If they are, continue playing
         // the animation, otherwise stop
         return armorList.contains(Items.LAB_COAT) ? PlayState.CONTINUE : PlayState.STOP;
+    }*/
+
+    private PlayState predicate(AnimationState state){
+        state.getController().setAnimation(RawAnimation.begin().then("idle", Animation.LoopType.LOOP));
+        return PlayState.CONTINUE;
     }
+
 
     // All you need to do here is add your animation controllers to the
     // AnimationData
     @Override
-    public void registerControllers(AnimatableManager.ControllerRegistrar controllerRegistrar) {
-        controllerRegistrar.add(new AnimationController<>(this, "controller", 20, this::predicate));
+    public void registerControllers(AnimatableManager.ControllerRegistrar controllers) {
+        controllers.add(new AnimationController<>(this, "controller", 0, this::predicate));
+    }
+
+    @Override
+    public AnimatableInstanceCache getAnimatableInstanceCache() {
+        return this.cache;
+    }
+
+    @Override
+    public void createRenderer(Consumer<Object> consumer) {
+
+    }
+
+    @Override
+    public Supplier<Object> getRenderProvider() {
+        return this.renderProvider;
     }
 }
--- src/main/java/be/uantwerpen/minelabs/renderer/LabCoatRenderer.java
+++ src/main/java/be/uantwerpen/minelabs/renderer/LabCoatRenderer.java
@@ -7,14 +7,5 @@ import software.bernie.geckolib.renderer.GeoArmorRenderer;
 public class LabCoatRenderer extends GeoArmorRenderer<LabCoatArmorItem> {
     public LabCoatRenderer() {
         super(new LabCoatModel());
-
-        this.headBone = "armorHead";
-        this.bodyBone = "armorBody";
-        this.rightArmBone = "armorRightArm";
-        this.leftArmBone = "armorLeftArm";
-        this.rightLegBone = "armorRightLeg";
-        this.leftLegBone = "armorLeftLeg";
-        this.rightBootBone = "armorLeftBoot";
-        this.leftLegBone = "armorRightBoot";
     }
 }
