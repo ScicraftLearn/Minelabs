f016065547e9bec2ea50cc8dfdcd4a23e3006bf0
Joey De Pauw <joeydepauw@gmail.com>
2023-05-28 17:36:51 +0200

    fix atom texture in LCT, fix LCT button (fixes #718)

--- src/client/java/be/minelabs/client/gui/screen/Screens.java
+++ src/client/java/be/minelabs/client/gui/screen/Screens.java
@@ -12,11 +12,6 @@ public class Screens {
         HandledScreens.register(ScreenHandlers.IONIC_SCREEN_HANDLER, IonicScreen::new);
         HandledScreens.register(ScreenHandlers.LAB_CHEST_SCREEN_HANDLER, LabChestScreen::new);
         HandledScreens.register(ScreenHandlers.CHARGED_POINT_SCREEN_HANDLER, ChargedPointScreen::new);
-
-        ScreenEvents.BEFORE_INIT.register((a, screen, b, c) -> {
-            if (screen instanceof LewisScreen)
-                ScreenMouseEvents.afterMouseRelease(screen).register((d, mouseX, mouseY, e) -> ((LewisScreen) screen).getButtonWidget().onClick(mouseX, mouseY));
-        });
     }
 
 }
