5ccaac33784c2395877924d9daaa01ee4dab41b8
pixar02 <pixar.pf@gmail.com>
2023-05-28 16:27:00 +0200

    updated gas color

--- src/client/java/be/minelabs/client/item/Erlenmeyers.java
+++ src/client/java/be/minelabs/client/item/Erlenmeyers.java
@@ -18,36 +18,36 @@ public class Erlenmeyers {
         // TODO - enchantment visuals voor zeldzame stoffen
 
         // GASSES
-        registerErlenmeyer(Items.ERLENMEYER_O2, 0xAFAFAF, 0);
-        registerErlenmeyer(Items.ERLENMEYER_N2, 0xAFAFAF, 0);
-        registerErlenmeyer(Items.ERLENMEYER_CH4, 0xAFAFAF, 0);
-        registerErlenmeyer(Items.ERLENMEYER_H2, 0xAFAFAF, 0);
-        registerErlenmeyer(Items.ERLENMEYER_NO, 0xAFAFAF, 0);
+        registerErlenmeyer(Items.ERLENMEYER_O2, 0xCFCFFF, 0);
+        registerErlenmeyer(Items.ERLENMEYER_N2, 0xCFCFFF, 0);
+        registerErlenmeyer(Items.ERLENMEYER_CH4, 0xCFCFFF, 0);
+        registerErlenmeyer(Items.ERLENMEYER_H2, 0xCFCFFF, 0);
+        registerErlenmeyer(Items.ERLENMEYER_NO, 0xCFCFFF, 0);
         registerErlenmeyer(Items.ERLENMEYER_NO2, 0x991c00, 0);
         registerErlenmeyer(Items.ERLENMEYER_Cl2, 0xE8F48C, 0);
-        registerErlenmeyer(Items.ERLENMEYER_CO2, 0xAFAFAF, 0);
-        registerErlenmeyer(Items.ERLENMEYER_CO, 0xAFAFAF, 0);
-        registerErlenmeyer(Items.ERLENMEYER_NH3, 0xAFAFAF, 0);
-        registerErlenmeyer(Items.ERLENMEYER_N2O, 0xAFAFAF, 0);
-        registerErlenmeyer(Items.ERLENMEYER_HCl, 0xAFAFAF, 0);
-        registerErlenmeyer(Items.ERLENMEYER_He, 0xAFAFAF, 0);
-        registerErlenmeyer(Items.ERLENMEYER_Ne, 0xAFAFAF, 0);
-        registerErlenmeyer(Items.ERLENMEYER_Ar, 0xAFAFAF, 0);
-        registerErlenmeyer(Items.ERLENMEYER_CL2O,0xAFAFAF, 0);
-        registerErlenmeyer(Items.ERLENMEYER_H2CO3,0xAFAFAF, 0);
-        registerErlenmeyer(Items.ERLENMEYER_CH4S,0xAFAFAF, 0);
-        registerErlenmeyer(Items.ERLENMEYER_CH2O,0xAFAFAF, 0);
-        registerErlenmeyer(Items.ERLENMEYER_BH3,0xFFFFFF, 0);
-        registerErlenmeyer(Items.ERLENMEYER_HF,0xFFFFFF, 0);
-        registerErlenmeyer(Items.ERLENMEYER_SIH4,0xFFFFFF, 0);
-        registerErlenmeyer(Items.ERLENMEYER_PH3,0xFFFFFF, 0);
-        registerErlenmeyer(Items.ERLENMEYER_H2S,0xFFFFFF, 0);
-        registerErlenmeyer(Items.ERLENMEYER_CF4,0xFFFFFF, 0);
-        registerErlenmeyer(Items.ERLENMEYER_BF3,0xFFFFFF, 0);
-        registerErlenmeyer(Items.ERLENMEYER_BCL3,0xFFFFFF, 0);
-        registerErlenmeyer(Items.ERLENMEYER_SO2,0xFFFFFF, 0);
-        registerErlenmeyer(Items.ERLENMEYER_CLF,0xFFFFFF, 0);
-        registerErlenmeyer(Items.ERLENMEYER_F2,0xFFFFFF, 0);
+        registerErlenmeyer(Items.ERLENMEYER_CO2, 0xCFCFFF, 0);
+        registerErlenmeyer(Items.ERLENMEYER_CO, 0xCFCFFF, 0);
+        registerErlenmeyer(Items.ERLENMEYER_NH3, 0xCFCFFF, 0);
+        registerErlenmeyer(Items.ERLENMEYER_N2O, 0xCFCFFF, 0);
+        registerErlenmeyer(Items.ERLENMEYER_HCl, 0xCFCFFF, 0);
+        registerErlenmeyer(Items.ERLENMEYER_He, 0xCFCFFF, 0);
+        registerErlenmeyer(Items.ERLENMEYER_Ne, 0xCFCFFF, 0);
+        registerErlenmeyer(Items.ERLENMEYER_Ar, 0xCFCFFF, 0);
+        registerErlenmeyer(Items.ERLENMEYER_CL2O,0xCFCFFF, 0);
+        registerErlenmeyer(Items.ERLENMEYER_H2CO3,0xCFCFFF, 0);
+        registerErlenmeyer(Items.ERLENMEYER_CH4S,0xCFCFFF, 0);
+        registerErlenmeyer(Items.ERLENMEYER_CH2O,0xCFCFFF, 0);
+        registerErlenmeyer(Items.ERLENMEYER_BH3,0xCFCFFF, 0);
+        registerErlenmeyer(Items.ERLENMEYER_HF,0xCFCFFF, 0);
+        registerErlenmeyer(Items.ERLENMEYER_SIH4,0xCFCFFF, 0);
+        registerErlenmeyer(Items.ERLENMEYER_PH3,0xCFCFFF, 0);
+        registerErlenmeyer(Items.ERLENMEYER_H2S,0xCFCFFF, 0);
+        registerErlenmeyer(Items.ERLENMEYER_CF4,0xCFCFFF, 0);
+        registerErlenmeyer(Items.ERLENMEYER_BF3,0xCFCFFF, 0);
+        registerErlenmeyer(Items.ERLENMEYER_BCL3,0xCFCFFF, 0);
+        registerErlenmeyer(Items.ERLENMEYER_SO2,0xCFCFFF, 0);
+        registerErlenmeyer(Items.ERLENMEYER_CLF,0xCFCFFF, 0);
+        registerErlenmeyer(Items.ERLENMEYER_F2,0xCFCFFF, 0);
 
         //Fluids
         registerErlenmeyer(Items.ERLENMEYER_HNO3, 0xFFCC33, 0);
