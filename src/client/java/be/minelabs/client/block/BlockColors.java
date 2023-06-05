66572e306c365d9ad13ceb38b83fb09aa62d3770
Joey De Pauw <joeydepauw@gmail.com>
2023-06-05 18:10:38 +0200

    undo color provider experiment for quantum field transparency.

--- src/client/java/be/minelabs/client/block/BlockColors.java
+++ src/client/java/be/minelabs/client/block/BlockColors.java
@@ -10,13 +10,5 @@ public class BlockColors {
 
     public static void onInitializeClient() {
         ColorProviderRegistry.BLOCK.register((state, view, pos, tintIndex) -> 0x3495eb, Blocks.LAB_SINK);
-//        ColorProviderRegistry.BLOCK.register((state, view, pos, tintIndex) -> {
-//            float age = (float) QuantumfieldBlock.getAge(state) / QuantumfieldBlock.MAX_AGE;
-//            // linear from 0 to 1 at 0.5 and then back down to 0
-//            float intensity = 1f - (float) Math.abs(age - 0.5) * 2;
-//            int color = MathHelper.lerp(intensity, 150, 255);
-//            // alpha is not actually used, so just set it to max
-//            return ColorHelper.Argb.getArgb(255, color, color, color);
-//        }, Blocks.GLUON_QUANTUMFIELD);
     }
 }
