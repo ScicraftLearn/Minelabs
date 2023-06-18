b00fd75cc0cb3f1afac72fc0e6bbab628a5a24e2
Joey De Pauw <joeydepauw@gmail.com>
2023-06-18 11:21:55 +0200

    use Inventories nbt utility for OrderedInventory. Closes #757

--- src/main/java/be/minelabs/inventory/OrderedInventory.java
+++ src/main/java/be/minelabs/inventory/OrderedInventory.java
@@ -1,8 +1,11 @@
 package be.minelabs.inventory;
 
+import com.mojang.datafixers.DataFixUtils;
+import net.minecraft.inventory.Inventories;
 import net.minecraft.inventory.SimpleInventory;
 import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NbtCompound;
+import net.minecraft.nbt.NbtElement;
 import net.minecraft.nbt.NbtList;
 
 /**
@@ -20,30 +23,12 @@ public class OrderedInventory extends SimpleInventory {
 
     @Override
     public void readNbtList(NbtList nbtList) {
-        int i;
-        for (i = 0; i < this.size(); ++i) {
-            this.setStack(i, ItemStack.EMPTY);
-        }
-        for (i = 0; i < nbtList.size(); ++i) {
-            NbtCompound nbtCompound = nbtList.getCompound(i);
-            int j = nbtCompound.getByte("Slot") & 0xFF;
-            if (j < 0 || j >= this.size()) continue;
-            this.setStack(j, ItemStack.fromNbt(nbtCompound));
-        }
+        Inventories.readNbt(DataFixUtils.make(new NbtCompound(), c -> c.put("Items", nbtList)), stacks);
     }
 
     @Override
     public NbtList toNbtList() {
-        NbtList nbtList = new NbtList();
-        for (int i = 0; i < this.size(); ++i) {
-            ItemStack itemStack = this.getStack(i);
-            if (itemStack.isEmpty()) continue;
-            NbtCompound nbtCompound = new NbtCompound();
-            nbtCompound.putByte("Slot", (byte)i);
-            itemStack.writeNbt(nbtCompound);
-            nbtList.add(nbtCompound);
-        }
-        return nbtList;
+        return Inventories.writeNbt(new NbtCompound(), stacks, true).getList("Items", NbtElement.COMPOUND_TYPE);
     }
 
 }
