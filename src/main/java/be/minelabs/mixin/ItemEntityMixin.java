5ac8a791d6e16ffc78fdd2731253da93af0e1aa7
pixar02 <pixar.pf@gmail.com>
2023-07-18 18:58:05 +0200

    clean

--- src/main/java/be/minelabs/mixin/ItemEntityMixin.java
+++ src/main/java/be/minelabs/mixin/ItemEntityMixin.java
@@ -12,6 +12,7 @@ import net.minecraft.nbt.NbtCompound;
 import org.jetbrains.annotations.Nullable;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
@@ -51,6 +52,7 @@ public abstract class ItemEntityMixin {
         }
     }
 
+    @Unique
     private Set<ItemStack> getPack(PlayerInventory inventory){
         Set<ItemStack> set = new HashSet<>();
         for (int i = 0; i < inventory.size(); i++) {
