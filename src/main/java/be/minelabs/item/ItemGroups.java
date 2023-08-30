7396f1752201246f3acfb00ecd057ca37a125340
pixar02 <pixar.pf@gmail.con>
2023-08-30 16:47:05 +0200

    Removing erlenmeyer stand from creative menu

--- src/main/java/be/minelabs/item/ItemGroups.java
+++ src/main/java/be/minelabs/item/ItemGroups.java
@@ -81,7 +81,6 @@ public class ItemGroups {
                 entries.add(new ItemStack(Items.DUST_ALN));
                 entries.add(new ItemStack(Items.DUST_MGO));
                 entries.add(new ItemStack(Items.DUST_SO3));
-                entries.add(new ItemStack(Items.ERLENMEYER_STAND));
                 entries.add(new ItemStack(Items.ERLENMEYER));
                 entries.add(new ItemStack(Items.ERLENMEYER_O2));
                 entries.add(new ItemStack(Items.ERLENMEYER_N2));
