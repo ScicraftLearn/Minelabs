eaf6b5f9da73b5c870a96a1a715eb5c147ea5ba3
pixar02 <pixar.pf@gmail.con>
2023-09-12 11:44:58 +0200

    datatracker

--- src/main/java/be/minelabs/entity/ChargedEntity.java
+++ src/main/java/be/minelabs/entity/ChargedEntity.java
@@ -1,18 +1,14 @@
 package be.minelabs.entity;
 
-import be.minelabs.Minelabs;
-import be.minelabs.item.Items;
+import be.minelabs.block.Blocks;
 import net.minecraft.block.piston.PistonBehavior;
 import net.minecraft.entity.*;
 import net.minecraft.entity.damage.DamageSource;
-import net.minecraft.entity.mob.MobEntity;
-import net.minecraft.entity.projectile.ProjectileEntity;
+import net.minecraft.entity.data.DataTracker;
+import net.minecraft.entity.data.TrackedData;
+import net.minecraft.entity.data.TrackedDataHandlerRegistry;
 import net.minecraft.entity.projectile.thrown.ThrownEntity;
-import net.minecraft.inventory.Inventory;
-import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NbtCompound;
-import net.minecraft.util.Arm;
-import net.minecraft.util.ItemScatterer;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Box;
 import net.minecraft.util.math.Vec3d;
@@ -21,7 +17,7 @@ import net.minecraft.world.World;
 import java.util.List;
 
 public class ChargedEntity extends ThrownEntity {
-    private int charge = 0;
+    private static final TrackedData<Integer> CHARGE = DataTracker.registerData(ChargedEntity.class, TrackedDataHandlerRegistry.INTEGER);
 
     private float mass;
 
@@ -34,7 +30,7 @@ public class ChargedEntity extends ThrownEntity {
     public ChargedEntity(World world, BlockPos pos, int charge, float mass) {
         this(Entities.CHARGED_ENTITY, world);
         setPosition(pos.getX() + 0.5f, pos.getY() + 0.5f, pos.getZ() + 0.5f);
-        this.charge = charge;
+        setCharge(charge);
         this.mass = mass;
     }
 
@@ -63,12 +59,24 @@ public class ChargedEntity extends ThrownEntity {
     @Override
     public void tick() {
         if (!world.isClient) {
+            Iterable<BlockPos> positions = BlockPos.iterateOutwards(getBlockPos(), e_radius, e_radius, e_radius);
+            for (BlockPos pos : positions) {
+                if (world.getBlockState(pos).isOf(Blocks.TIME_FREEZE_BLOCK)) {
+                    //"Force" a stop
+                    updateVelocity(5f, Vec3d.ZERO);
+
+                    super.tick();
+                    return;
+                }
+            }
+
+
             List<Entity> entities = world.getOtherEntities(this,
                     Box.of(this.getPos(), e_radius, e_radius, e_radius), entity -> entity instanceof ChargedEntity);
 
             for (Entity entity : entities) {
                 if (entity instanceof ChargedEntity chargedEntity) {
-                    double force = charge * chargedEntity.getCharge() / squaredDistanceTo(chargedEntity);
+                    double force = getCharge() * chargedEntity.getCharge() / squaredDistanceTo(chargedEntity);
                     Vec3d vector = getPos().subtract(chargedEntity.getPos());
                     vector = vector.multiply(force);
                     updateVelocity(0.003f, vector.normalize());
@@ -81,24 +89,25 @@ public class ChargedEntity extends ThrownEntity {
     @Override
     protected void initDataTracker() {
         this.setNoGravity(true);
+        dataTracker.startTracking(CHARGE, 0);
     }
 
     @Override
     public void readCustomDataFromNbt(NbtCompound nbt) {
-        charge = nbt.getInt("charge");
+        dataTracker.set(CHARGE, nbt.getInt("charge"));
     }
 
     @Override
     public void writeCustomDataToNbt(NbtCompound nbt) {
-        nbt.putInt("charge", charge);
+        nbt.putInt("charge", dataTracker.get(CHARGE));
     }
 
     public int getCharge() {
-        return charge;
+        return dataTracker.get(CHARGE);
     }
 
     public void setCharge(int charge) {
-        this.charge = charge;
+        dataTracker.set(CHARGE, charge);
     }
 
     @Override
