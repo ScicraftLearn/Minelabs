899ec0acb04af72157059eace80deda0ccf4b9b5
pixar02 <pixar.pf@gmail.com>
2023-10-27 08:46:45 +0200

    some Ionic rei integration

--- src/client/java/be/minelabs/client/integration/rei/categories/IonicCategory.java
+++ src/client/java/be/minelabs/client/integration/rei/categories/IonicCategory.java
@@ -3,9 +3,11 @@ package be.minelabs.client.integration.rei.categories;
 import be.minelabs.Minelabs;
 import be.minelabs.block.Blocks;
 import be.minelabs.client.integration.rei.displays.IonicDisplay;
+import me.shedaniel.math.Point;
 import me.shedaniel.math.Rectangle;
 import me.shedaniel.rei.api.client.gui.Renderer;
 import me.shedaniel.rei.api.client.gui.widgets.Widget;
+import me.shedaniel.rei.api.client.gui.widgets.Widgets;
 import me.shedaniel.rei.api.client.registry.display.DisplayCategory;
 import me.shedaniel.rei.api.common.category.CategoryIdentifier;
 import me.shedaniel.rei.api.common.display.basic.BasicDisplay;
@@ -13,6 +15,7 @@ import me.shedaniel.rei.api.common.util.EntryStacks;
 import net.minecraft.text.Text;
 import net.minecraft.util.Identifier;
 
+import java.util.LinkedList;
 import java.util.List;
 
 public class IonicCategory implements DisplayCategory<BasicDisplay> {
@@ -38,12 +41,30 @@ public class IonicCategory implements DisplayCategory<BasicDisplay> {
 
     @Override
     public List<Widget> setupDisplay(BasicDisplay display, Rectangle bounds) {
-        // TODO
-        return DisplayCategory.super.setupDisplay(display, bounds);
+        final Point startPoint = new Point(bounds.getCenterX() - 105, bounds.getCenterY() - 45);
+        List<Widget> widgets = new LinkedList<>();
+        widgets.add(Widgets.createTexturedWidget(TEXTURE,
+                new Rectangle(startPoint.x, startPoint.y, 200, 84)));
+        // TODO ingredients
+
+        widgets.add(Widgets.createSlot(new Point(startPoint.x + 176, startPoint.y + 40)).disableBackground()
+                .entries(display.getOutputEntries().get(0)));
+        IonicDisplay disp = (IonicDisplay) display;
+        if (!disp.getContainer().isEmpty()) {
+            widgets.add(Widgets.createSlot(new Point(startPoint.x + 178, startPoint.y + 86)).disableBackground()
+                    .entries(disp.getContainer()));
+        }
+
+        return widgets;
     }
 
     @Override
     public int getDisplayHeight() {
         return 90;
+    }
+
+    @Override
+    public int getDisplayWidth(BasicDisplay display) {
+        return 200;
     }
 }
--- src/client/java/be/minelabs/client/integration/rei/displays/IonicDisplay.java
+++ src/client/java/be/minelabs/client/integration/rei/displays/IonicDisplay.java
@@ -2,6 +2,7 @@ package be.minelabs.client.integration.rei.displays;
 
 import be.minelabs.client.integration.rei.categories.IonicCategory;
 import be.minelabs.client.integration.rei.util.Util;
+import be.minelabs.item.Items;
 import be.minelabs.recipe.ionic.IonicRecipe;
 import me.shedaniel.rei.api.common.category.CategoryIdentifier;
 import me.shedaniel.rei.api.common.display.basic.BasicDisplay;
@@ -13,18 +14,29 @@ import java.util.List;
 
 public class IonicDisplay extends BasicDisplay {
 
+    private final EntryIngredient container;
 
     public IonicDisplay(List<EntryIngredient> inputs, List<EntryIngredient> outputs) {
         super(inputs, outputs);
+        container = EntryIngredient.empty();
     }
 
     public IonicDisplay(IonicRecipe recipe) {
         super(Util.getInputList(recipe.getIngredients()),
                 List.of(EntryIngredient.of(EntryStacks.of(recipe.getOutput(DynamicRegistryManager.EMPTY)))));
+        if (recipe.needsContainer()) {
+            container = EntryIngredient.of(EntryStacks.of(Items.ERLENMEYER));
+        } else {
+            container = EntryIngredient.empty();
+        }
     }
 
     @Override
     public CategoryIdentifier<?> getCategoryIdentifier() {
         return IonicCategory.IONIC_CRAFTING;
+    }
+
+    public EntryIngredient getContainer() {
+        return container;
     }
 }
