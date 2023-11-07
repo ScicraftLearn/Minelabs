820210225a83b25cbfad0e2ab752cbdae92e11db
pixar02 <pixar.pf@gmail.com>
2023-11-07 17:17:05 +0100

    updated bohr recipes look

--- src/client/java/be/minelabs/client/integration/emi/recipes/BohrEmiRecipe.java
+++ src/client/java/be/minelabs/client/integration/emi/recipes/BohrEmiRecipe.java
@@ -15,10 +15,13 @@ import net.minecraft.recipe.Ingredient;
 import net.minecraft.util.Identifier;
 
 public class BohrEmiRecipe extends BasicEmiRecipe {
+    // TODO actual TEXTURE (just missing atm)
+    private static final Identifier BOHR_TEXTURE = new Identifier(Minelabs.MOD_ID, "textures/integration/bohr_image.png");
+
     public BohrEmiRecipe(AtomItem atomItem) {
         super(MinelabsEmiPlugin.BOHR_CATEGORY, new Identifier(Minelabs.MOD_ID,
                         "/" + MinelabsEmiPlugin.BOHR_CATEGORY.getId().getPath() + "/" + atomItem.getAtom().getName().toLowerCase()),
-                160, 90);
+                160, 80);
         Atom atom = atomItem.getAtom();
         inputs.add(EmiIngredient.of(Ingredient.ofStacks(new ItemStack(Items.ELECTRON)), atom.getAtomNumber()));
         inputs.add(EmiIngredient.of(Ingredient.ofStacks(new ItemStack(Items.NEUTRON)), atom.getInitialNeutrons()));
@@ -28,12 +31,14 @@ public class BohrEmiRecipe extends BasicEmiRecipe {
 
     @Override
     public void addWidgets(WidgetHolder widgets) {
-        widgets.addSlot(inputs.get(0), 90, 10);
-        widgets.addSlot(inputs.get(1), 90, 30);
-        widgets.addSlot(inputs.get(2), 90, 50);
+        widgets.addSlot(inputs.get(0), 85, 10);
+        widgets.addSlot(inputs.get(1), 85, 30);
+        widgets.addSlot(inputs.get(2), 85, 50);
 
-        widgets.addTexture(EmiTexture.EMPTY_ARROW, 110, 30);
+        widgets.addTexture(EmiTexture.EMPTY_ARROW, 105, 30);
 
-        widgets.addSlot(outputs.get(0), 135, 30).recipeContext(this);
+        widgets.addSlot(outputs.get(0), 130, 30).recipeContext(this);
+
+        widgets.addTexture(BOHR_TEXTURE, 10, 10, 60, 60, 10, 10);
     }
 }
