437e18b765ec389aff6236c30d27055e12656f3d
pixar02 <pixar.pf@gmail.com>
2023-11-07 22:45:59 +0100

    cleanup

--- src/main/java/be/minelabs/world/MinelabsGameRules.java
+++ src/main/java/be/minelabs/world/MinelabsGameRules.java
@@ -6,11 +6,11 @@ import net.minecraft.world.GameRules;
 
 public class MinelabsGameRules {
 
-    public static final GameRules.Key<GameRules.BooleanRule> ALLOW_DECAY =
-            GameRuleRegistry.register("allow_decay", GameRules.Category.UPDATES, GameRuleFactory.createBooleanRule(true));
+    public static final GameRules.Key<GameRules.BooleanRule> ALLOW_DECAY = GameRuleRegistry.register(
+            "allow_decay", GameRules.Category.UPDATES, GameRuleFactory.createBooleanRule(true));
 
-    public static final GameRules.Key<GameRules.IntRule> E_RADIUS =
-            GameRuleRegistry.register("e_radius", GameRules.Category.UPDATES, GameRuleFactory.createIntRule(12));
+    public static final GameRules.Key<GameRules.IntRule> E_RADIUS = GameRuleRegistry.register("e_radius",
+            GameRules.Category.UPDATES, GameRuleFactory.createIntRule(12));
 
 
     public static void onInitialize() {
