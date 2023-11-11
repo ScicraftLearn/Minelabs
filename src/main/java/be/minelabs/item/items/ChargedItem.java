d66e068f10021f1d970f8aba74cddee8731ce37a
pixar02 <pixar.pf@gmail.com>
2023-11-11 17:39:14 +0100

    fixes #805

--- src/main/java/be/minelabs/item/items/ChargedItem.java
+++ src/main/java/be/minelabs/item/items/ChargedItem.java
@@ -1,5 +1,6 @@
 package be.minelabs.item.items;
 
+import be.minelabs.entity.projectile.thrown.ChargedEntity;
 import be.minelabs.entity.projectile.thrown.ParticleEntity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.entity.projectile.ProjectileEntity;
@@ -11,7 +12,10 @@ import net.minecraft.util.ActionResult;
 import net.minecraft.util.Hand;
 import net.minecraft.util.TypedActionResult;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.Box;
+import net.minecraft.util.math.Direction;
 import net.minecraft.world.World;
+import net.minecraft.world.event.GameEvent;
 
 public class ChargedItem extends Item {
 
@@ -27,11 +31,17 @@ public class ChargedItem extends Item {
      */
     @Override
     public ActionResult useOnBlock(ItemUsageContext context) {
-        if (!context.getWorld().isClient) {
-            ProjectileEntity entity = createPlacedEntity(context.getWorld(), context.getBlockPos().offset(context.getSide()), context.getStack());
-            context.getWorld().spawnEntity(entity);
+        World world = context.getWorld();
+        PlayerEntity player = context.getPlayer();
+        if (!canPlaceEntity(world, context.getBlockPos(), context.getSide(), player, context.getStack())) {
+            return ActionResult.FAIL;
         }
-        context.getPlayer().incrementStat(Stats.USED.getOrCreateStat(this));
+        if (!world.isClient) {
+            ProjectileEntity entity = createPlacedEntity(world, context.getBlockPos().offset(context.getSide()), context.getStack());
+            world.spawnEntity(entity);
+            world.emitGameEvent(player, GameEvent.ENTITY_PLACE, entity.getPos());
+        }
+        player.incrementStat(Stats.USED.getOrCreateStat(this));
         if (!context.getPlayer().getAbilities().creativeMode) {
             context.getStack().decrement(1);
         }
@@ -61,6 +71,11 @@ public class ChargedItem extends Item {
         }
 
         return TypedActionResult.success(stack, world.isClient());
+    }
+
+    private boolean canPlaceEntity(World world, BlockPos pos, Direction direction, PlayerEntity player, ItemStack stack) {
+        boolean bl = player.canPlaceOn(pos, direction, stack); // Allows ADVENTURE mode checking
+        return bl && world.getEntitiesByClass(ChargedEntity.class, Box.of(pos.offset(direction).toCenterPos(), 0.5, 0.5, 0.5), chargedEntity -> true).isEmpty();
     }
 
     protected ProjectileEntity createThrownEntity(PlayerEntity user, World world, ItemStack stack) {
