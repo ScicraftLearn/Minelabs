eab2f731143cc852682eb92cc12285b75a40df8f
Joey De Pauw <joeydepauw@gmail.com>
2023-11-12 16:37:03 +0100

    bohr particle good enough

--- src/client/java/be/minelabs/client/particle/BohrPlateParticle.java
+++ src/client/java/be/minelabs/client/particle/BohrPlateParticle.java
@@ -16,8 +16,8 @@ class BohrPlateParticle extends SpriteBillboardParticle {
         this.velocityZ = velocityZ * 0.01f + vz;
         this.angle = angle;
         this.prevAngle = angle;
-        setAlpha(0.15f);
-        maxAge = 14;
+        setAlpha(0.4f);
+        maxAge = 15;
         scale *= 0.5f;
     }
 
@@ -38,9 +38,16 @@ class BohrPlateParticle extends SpriteBillboardParticle {
         public Particle createParticle(DefaultParticleType parameters, ClientWorld world, double x, double y, double z, double vx, double vy, double vz) {
             // Note: usually vx, vy and vz should be kept at zero and this factory provides the correct velocity.
             Random random = Random.create();
-            Vector3f offset = new Vector3f(random.nextFloat() - 0.5f, random.nextFloat() - 0.5f, random.nextFloat() - 0.5f).normalize().mul(0.35f);
+            Vector3f offset = new Vector3f(random.nextFloat() - 0.5f, random.nextFloat() - 0.5f, random.nextFloat() - 0.5f).normalize().mul(0.3f);
             Vector3f velocity = new Vector3f(offset).mul(-0.05f);
-            float angle = (float) (random.nextFloat() * 2 * Math.PI);
+
+            // random angles
+//            float angle = (float) (random.nextFloat() * 2 * Math.PI);
+
+            // Heuristic to have the particle face the center
+            float offsetX = Math.abs(offset.x) > Math.abs(offset.y) ? offset.x : offset.y;
+            float angle = (float) (-Math.atan2(offsetX, offset.y));
+
             BohrPlateParticle particle = new BohrPlateParticle(world, x + offset.x, y + offset.y, z + offset.z, vx + velocity.x, vy + velocity.y, vz + velocity.z, angle);
             particle.setSprite(this.spriteProvider);
             return particle;
