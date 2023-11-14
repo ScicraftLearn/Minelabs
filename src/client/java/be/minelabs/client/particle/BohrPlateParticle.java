c4dcb3f588c34cfc228e4be77356163f15096cc9
Bert Jorissen <info@bertjorissen.be>
2023-11-14 12:16:09 +0100

    BhorPlateParticles: render them inwards by projecting them on a normal plane. The size is given by the z-position of the particel

--- src/client/java/be/minelabs/client/particle/BohrPlateParticle.java
+++ src/client/java/be/minelabs/client/particle/BohrPlateParticle.java
@@ -1,5 +1,7 @@
 package be.minelabs.client.particle;
 
+import net.fabricmc.loader.impl.lib.sat4j.core.Vec;
+import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.particle.*;
 import net.minecraft.client.world.ClientWorld;
 import net.minecraft.particle.DefaultParticleType;
@@ -8,7 +10,7 @@ import org.jetbrains.annotations.Nullable;
 import org.joml.Vector3f;
 
 class BohrPlateParticle extends SpriteBillboardParticle {
-    protected BohrPlateParticle(ClientWorld clientWorld, double x, double y, double z, double vx, double vy, double vz, float angle) {
+    protected BohrPlateParticle(ClientWorld clientWorld, double x, double y, double z, double vx, double vy, double vz, float angle, float scale) {
         super(clientWorld, x, y, z, vx, vy, vz);
         this.velocityMultiplier = 0.94f;
         this.velocityX = velocityX * 0.01f + vx;
@@ -18,7 +20,7 @@ class BohrPlateParticle extends SpriteBillboardParticle {
         this.prevAngle = angle;
         setAlpha(0.4f);
         maxAge = 15;
-        scale *= 0.5f;
+        this.scale *= scale;
     }
 
     @Override
@@ -41,14 +43,41 @@ class BohrPlateParticle extends SpriteBillboardParticle {
             Vector3f offset = new Vector3f(random.nextFloat() - 0.5f, random.nextFloat() - 0.5f, random.nextFloat() - 0.5f).normalize().mul(0.3f);
             Vector3f velocity = new Vector3f(offset).mul(-0.05f);
 
+            // Get the position of the camera
+            Vector3f cameraPos = MinecraftClient.getInstance().player.getClientCameraPosVec(0).toVector3f();
+
+            // Get the position of the center of the HUD
+            Vector3f hudPos = new Vector3f((float) x, (float) y, (float) z);
+
+            // Get the normal on the plane on which to project the particle
+            Vector3f normalPlane = hudPos.sub(cameraPos).normalize();
+
+            // Reference, what is 'up' on the plane, this - better to use Pitch/Yaw and all those things
+            Vector3f yVec = new Vector3f(0,1,0);
+
+            // Project on to the plane
+            Vector3f projy = yVec.sub(normalPlane.mul(yVec.dot(normalPlane), new Vector3f()), new Vector3f()).normalize();
+
+            // Obtain the x-axis on the plane by some cross product
+            Vector3f projx = normalPlane.cross(projy, new Vector3f());
+
+            // Project the particle on the plane
+            Vector3f projp = offset.sub(normalPlane.mul(offset.dot(normalPlane), new Vector3f()), new Vector3f()).normalize();
+
             // random angles
 //            float angle = (float) (random.nextFloat() * 2 * Math.PI);
 
             // Heuristic to have the particle face the center
-            float offsetX = Math.abs(offset.x) > Math.abs(offset.y) ? offset.x : offset.y;
-            float angle = (float) (-Math.atan2(offsetX, offset.y));
+            //float offsetX = Math.abs(offset.x) > Math.abs(offset.y) ? offset.x : offset.y;
+            //float angle = (float) (-Math.atan2(offsetX, offset.y));
 
-            BohrPlateParticle particle = new BohrPlateParticle(world, x + offset.x, y + offset.y, z + offset.z, vx + velocity.x, vy + velocity.y, vz + velocity.z, angle);
+            // we can maybe adjust the scale with the z-position
+            float scale = (offset.dot(normalPlane) + 1f) * .25f;
+
+            // Obtain the angle: the dot product can't get the sign, that's why the x-part is also there  to give the sign
+            float angle = (float) Math.acos(projy.dot(projp)) * Math.signum(projp.dot(projx));
+
+            BohrPlateParticle particle = new BohrPlateParticle(world, x + offset.x, y + offset.y, z + offset.z, vx + velocity.x, vy + velocity.y, vz + velocity.z, angle, scale);
             particle.setSprite(this.spriteProvider);
             return particle;
         }
