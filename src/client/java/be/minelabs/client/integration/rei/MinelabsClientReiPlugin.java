93717ddab638be2ad33dd8685982bb7b49a3e74f
pixar02 <pixar.pf@gmail.com>
2024-02-11 11:43:12 +0100

    Bohr/AtomCrafting to REI

--- src/client/java/be/minelabs/client/integration/rei/MinelabsClientReiPlugin.java
+++ src/client/java/be/minelabs/client/integration/rei/MinelabsClientReiPlugin.java
@@ -3,12 +3,15 @@ package be.minelabs.client.integration.rei;
 import be.minelabs.block.Blocks;
 import be.minelabs.client.gui.screen.IonicScreen;
 import be.minelabs.client.gui.screen.LewisScreen;
+import be.minelabs.client.integration.rei.categories.BohrCategory;
 import be.minelabs.client.integration.rei.categories.CoulombInfoCategory;
 import be.minelabs.client.integration.rei.categories.IonicCategory;
 import be.minelabs.client.integration.rei.categories.LewisCategory;
+import be.minelabs.client.integration.rei.displays.BohrDisplay;
 import be.minelabs.client.integration.rei.displays.CoulombInfoDisplay;
 import be.minelabs.client.integration.rei.displays.IonicDisplay;
 import be.minelabs.client.integration.rei.displays.LewisDisplay;
+import be.minelabs.item.Items;
 import be.minelabs.item.items.AtomItem;
 import be.minelabs.recipe.ionic.IonicRecipe;
 import be.minelabs.recipe.lewis.MoleculeRecipe;
@@ -37,14 +40,22 @@ public class MinelabsClientReiPlugin implements REIClientPlugin {
         registry.addWorkstations(IonicCategory.IONIC_CRAFTING, EntryStacks.of(Blocks.IONIC_BLOCK));
 
         registry.add(new CoulombInfoCategory());
+
+        registry.add(new BohrCategory());
+        registry.addWorkstations(BohrCategory.ATOM_CRAFTING, EntryStacks.of(Blocks.BOHR_BLUEPRINT));
     }
 
     @Override
     public void registerDisplays(DisplayRegistry registry) {
         registry.registerRecipeFiller(MoleculeRecipe.class, MoleculeRecipe.MoleculeRecipeType.INSTANCE, LewisDisplay::new);
         registry.registerRecipeFiller(IonicRecipe.class, IonicRecipe.IonicRecipeType.INSTANCE, IonicDisplay::new);
+
         for (Map.Entry<Identifier, CoulombData> entry : CoulombResource.INSTANCE.getResourceData().entrySet()) {
             registry.add(new CoulombInfoDisplay(entry.getValue(), entry.getKey()));
+        }
+
+        for (AtomItem item : Items.ATOMS){
+            registry.add(new BohrDisplay(item));
         }
     }
 
@@ -59,6 +70,6 @@ public class MinelabsClientReiPlugin implements REIClientPlugin {
     @Override
     public void registerExclusionZones(ExclusionZones zones) {
         // Might have to rethink these numbers
-        zones.register(LewisScreen.class, screen -> List.of(new Rectangle(177, 90, 20,42)));
+        zones.register(LewisScreen.class, screen -> List.of(new Rectangle(177, 90, 20, 42)));
     }
 }
