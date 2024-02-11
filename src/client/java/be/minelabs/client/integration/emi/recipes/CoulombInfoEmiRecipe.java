12694b10071d8d563021591717e6aebedb7ee2c4
pixar02 <pixar.pf@gmail.com>
2024-02-11 14:30:30 +0100

    improvements to CoulombInfo for EMI

--- src/client/java/be/minelabs/client/integration/emi/recipes/CoulombInfoEmiRecipe.java
+++ src/client/java/be/minelabs/client/integration/emi/recipes/CoulombInfoEmiRecipe.java
@@ -38,14 +38,15 @@ public class CoulombInfoEmiRecipe extends BasicEmiRecipe {
     public void addWidgets(WidgetHolder widgets) {
         // TODO CHANGE TO TRANSLATE KEYS
         // TODO REMOVE TEMPLATE FILE (data/minelabs/science/coulomb/template.json)
+        int text_color = 0xffffffff;
 
         widgets.addSlot(inputs.get(0), 15, 20);
-        widgets.addText(Text.of("Charge:    " + data.charge.toString()), 60, 20, 1, false);
-        widgets.addText(Text.of("Mass:      " + data.mass.toString()), 60, 30, 1, false);
-        widgets.addText(Text.of(data.stable ? "Stable" : "Unstable"), 60, 40, 1, false);
+        widgets.addText(Text.of("Charge:    " + data.charge.toString()), 60, 20, text_color, true);
+        widgets.addText(Text.of("Mass:      " + data.mass.toString()), 60, 30, text_color, true);
+        widgets.addText(Text.of(data.stable ? "Stable" : "Unstable"), 60, 40, text_color, true);
 
-        widgets.addText(Text.of("======================"), 5, 60, 1, true);
-        widgets.addText(Text.of("Annihilation:"), 5, 75, 1, false);
+        widgets.addText(Text.of("======================"), 5, 60, text_color, true);
+        widgets.addText(Text.of("Annihilation:"), 5, 75, text_color, true);
         widgets.addSlot(inputs.get(0), 15, 85);
         widgets.addTexture(EmiTexture.PLUS, 40, 88);
         widgets.addSlot(inputs.get(1), 60, 85);
@@ -53,14 +54,14 @@ public class CoulombInfoEmiRecipe extends BasicEmiRecipe {
         widgets.addSlot(outputs.get(0), 110, 85);
 
         if (!data.stable) {
-            widgets.addText(Text.of("Decay:"), 5, 110, 1, false);
+            widgets.addText(Text.of("Decay:"), 5, 110, text_color, true);
             widgets.addSlot(inputs.get(0), 15, 120);
             widgets.addTexture(EmiTexture.EMPTY_ARROW, 40, 120);
 
             // Emi has a way to render chances, but we don't like the look
             widgets.addSlot(outputs.get(1), 70, 120);
-            widgets.addText(Text.of("Chance:"), 95, 120, 1, false);
-            widgets.addText(Text.of(data.decay_chance * 100 + "%"), 100, 130, 1, false);
+            widgets.addText(Text.of("Chance:"), 95, 120, text_color, true);
+            widgets.addText(Text.of(data.decay_chance * 100 + "%"), 100, 130, text_color, true);
         }
     }
 
